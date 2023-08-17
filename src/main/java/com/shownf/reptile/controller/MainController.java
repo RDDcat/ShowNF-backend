dce5067ceff3cc6a8f58e28a9284b8ce451237ac
Lee <lsmin476@naver.com>
2023-08-18 00:57:16 +0900

    |SM| 깃 커밋 테스트 0818

--- src/main/java/com/shownf/reptile/controller/MainController.java
+++ src/main/java/com/shownf/reptile/controller/MainController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @CrossOrigin("*")
 public class MainController {
@@ -14,8 +17,14 @@ public class MainController {
     }
 
     @GetMapping("/test")
-    public TestDAO healthtest(){
-        TestDAO testDAO = new TestDAO("nameIsTest", "test data for front-back connection", 12);
-        return testDAO;
+    public List<TestDAO> healthtest(){
+        List<TestDAO> testDAOS = new ArrayList<>();
+        TestDAO testDAO1 = new TestDAO("nameIsTest1", "test data for front-back connection", 1);
+        TestDAO testDAO2 = new TestDAO("nameIsTest2", "test data for front-back connection", 2);
+
+        testDAOS.add(testDAO1);
+        testDAOS.add(testDAO2);
+
+        return testDAOS;
     }
 }
