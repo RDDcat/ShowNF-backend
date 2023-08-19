d872095a95d0453c6f47f9dd8cdb7ad6802d8afb
Lee <lsmin476@naver.com>
2023-08-19 23:22:38 +0900

    |SM UPDATE| small bean 게시물 DTO 생성 0819

--- src/main/java/com/shownf/reptile/bean/small/CreatePostDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreatePostDTOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class CreatePostDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.entity.PostDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreatePostDTOBean {
+
+    public RequestPostDTO exec(PostDAO postDAO){
+        RequestPostDTO requestPostDTO = new RequestPostDTO();
+
+        // DTO 객체에 게시물 정보 넘기기
+        requestPostDTO.setPId(postDAO.getPId());
+        requestPostDTO.setUId(postDAO.getUId());
+        requestPostDTO.setTitle(postDAO.getTitle());
+        requestPostDTO.setContent(postDAO.getContent());
+        requestPostDTO.setCategory(postDAO.getCategory());
+        requestPostDTO.setUploadTime(postDAO.getUploadTime());
+
+        // DTO 반환
+        return requestPostDTO;
+    }
 }
