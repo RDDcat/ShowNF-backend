8476707e80494e8fde65b8075f11a8299aacc944
Lee <lsmin476@naver.com>
2023-08-19 23:25:52 +0900

    |SM UPDATE| Service 게시물 조회 기능 0819

--- src/main/java/com/shownf/reptile/service/PostService.java
+++ src/main/java/com/shownf/reptile/service/PostService.java
@@ -1,2 +1,30 @@
-package com.shownf.reptile.service;public class PostService {
+package com.shownf.reptile.service;
+
+import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.bean.GetPostBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    GetPostBean getPostBean;
+
+    @Autowired
+    public PostService(GetPostBean getPostBean) {
+        this.getPostBean = getPostBean;
+    }
+
+    // 게시물 조회
+    public RequestPostDTO getPostDAO(long pId, String uId){
+        return getPostBean.exec(pId, uId);
+    }
+
+
+    // 게시물 저장
+
+    // 게시물 수정
+
+    // 게시물 삭제
+
 }
