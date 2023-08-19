4f92dba6dcf4ec2eae1c2a7e3b5ceac7b90a878f
Lee <lsmin476@naver.com>
2023-08-19 23:26:09 +0900

    |SM UPDATE| Controller 게시물 조회 기능 0819

--- src/main/java/com/shownf/reptile/controller/PostController.java
+++ src/main/java/com/shownf/reptile/controller/PostController.java
@@ -1,2 +1,27 @@
-package com.shownf.reptile.controller;public class PostController {
+package com.shownf.reptile.controller;
+
+import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class PostController {
+
+    PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // 게시물 조회
+    @GetMapping("post/{pId}/user/{uId}")
+    public RequestPostDTO getPost(@PathVariable long pId, @PathVariable String uId){
+        return postService.getPostDAO(pId, uId);
+    }
 }
