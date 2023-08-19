da44e26ac095565c54556b75e41008ca6079d979
Lee <lsmin476@naver.com>
2023-08-19 23:21:29 +0900

    |SM UPDATE| smallbean 게시물 DAO 가져오기 0819

--- src/main/java/com/shownf/reptile/bean/small/GetPostDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostDAOBean.java
@@ -1,2 +1,21 @@
-package com.shownf.reptile.bean.small;public class GetPostDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.PostDAO;
+import com.shownf.reptile.repository.PostRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetPostDAOBean {
+    PostRepositoryJPA postRepositoryJPA;
+
+    @Autowired
+    public GetPostDAOBean(PostRepositoryJPA postRepositoryJPA) {
+        this.postRepositoryJPA = postRepositoryJPA;
+    }
+
+    // 게시물 객체 찾기
+    public PostDAO exec(Long pId){
+        return postRepositoryJPA.findById(pId).get();
+    }
 }
