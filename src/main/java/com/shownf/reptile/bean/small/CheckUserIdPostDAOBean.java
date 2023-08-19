7a8c0fb8d975224e00e4e7a631793b207a961dc0
Lee <lsmin476@naver.com>
2023-08-19 23:22:12 +0900

    |SM UPDATE| small bean 게시물 유저 Id 확인하기 0819

--- src/main/java/com/shownf/reptile/bean/small/CheckUserIdPostDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckUserIdPostDAOBean.java
@@ -1,2 +1,15 @@
-package com.shownf.reptile.bean.small;public class CheckUesrIdPostDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.PostDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckUserIdPostDAOBean {
+
+    // 게시물 uId 판별
+    public boolean exec(PostDAO postDAO, String uId){
+        if(postDAO.getUId().equals(uId))
+            return true;
+        return false;
+    }
 }
