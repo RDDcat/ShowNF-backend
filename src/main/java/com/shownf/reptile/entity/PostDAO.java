0731086952cf17e0b5647bfeb75ac64a289a0515
Lee <lsmin476@naver.com>
2023-08-19 23:24:36 +0900

    |SM UPDATE| DAO 게시물 0819

--- src/main/java/com/shownf/reptile/entity/PostDAO.java
+++ src/main/java/com/shownf/reptile/entity/PostDAO.java
@@ -1,2 +1,25 @@
-package com.shownf.reptile.entity;public class PostDAO {
+package com.shownf.reptile.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PostDAO {
+    @Id
+    long pId;
+    String uId;
+    String title;
+    String content;
+    String category;
+    LocalDateTime uploadTime;
 }
