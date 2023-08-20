9605f55fa429cd0e3b329dae0b8242287f9546d9
Lee <lsmin476@naver.com>
2023-08-20 23:17:10 +0900

    |SM UPDATE| small Bean 댓글 DAO 객체 생성하기 0820

--- src/main/java/com/shownf/reptile/bean/small/CreateCommentDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateCommentDAOBean.java
@@ -1,2 +1,29 @@
-package com.shownf.reptile.bean.small;public class CreateCommentDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestCommentSaveDTO;
+import com.shownf.reptile.entity.CommentDAO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreateCommentDAOBean {
+
+    // 댓글 저장시 DAO 생성
+    public CommentDAO exec(Long cId, RequestCommentSaveDTO requestCommentSaveDTO){
+
+        // 게시물 아이디
+        Long pId = requestCommentSaveDTO.getPId();
+
+        // 유저 아이디
+        String uId = requestCommentSaveDTO.getUId();
+
+        // 내용
+        String content = requestCommentSaveDTO.getContent();
+
+        // 시간
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        return  new CommentDAO(cId, pId, uId, content, localDateTime);
+    }
 }
