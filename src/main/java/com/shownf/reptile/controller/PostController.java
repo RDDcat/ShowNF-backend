320b77cae7d8f62eb1b18d17f3c6374bdf533a1f
Lee <lsmin476@naver.com>
2023-08-20 17:26:43 +0900

    |SM UPDATE| Controller 게시물 저장 기능 컨트롤러 0820

--- src/main/java/com/shownf/reptile/controller/PostController.java
+++ src/main/java/com/shownf/reptile/controller/PostController.java
@@ -1,12 +1,15 @@
 package com.shownf.reptile.controller;
 
 import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.DTO.RequestPostSaveDTO;
 import com.shownf.reptile.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @CrossOrigin("*")
@@ -23,5 +26,21 @@ public class PostController {
     @GetMapping("post/{pId}/user/{uId}")
     public RequestPostDTO getPost(@PathVariable long pId, @PathVariable String uId){
         return postService.getPostDAO(pId, uId);
+    }
+
+    // 게시물 저장
+    @PostMapping("post")
+    public ResponseEntity<Map<String, Object>> savePost(@RequestBody RequestPostSaveDTO requestPostSaveDTO){
+        Long pId = postService.savePostDAO(requestPostSaveDTO);
+
+        // HTTP 상태 변환
+        HttpStatus httpStatus = (pId != null) ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        // 메시지와 id 값 json 데이터로 반환
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("message", (pId != null) ? "Save Success" : "Save Fail");
+        requestMap.put("pId", pId);
+
+        return ResponseEntity.status(httpStatus).body(requestMap);
     }
 }
