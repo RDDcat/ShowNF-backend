0a23541bf92803acf4c2b3f53680e7c76501a95f
Lee <lsmin476@naver.com>
2023-08-21 01:23:54 +0900

    |SM UPDATE| small Bean 댓글 pId로 찾기 0821

--- src/main/java/com/shownf/reptile/bean/small/GetCommentsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetCommentsDAOBean.java
@@ -1,2 +1,25 @@
-package com.shownf.reptile.bean.small;public class GetCommentsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentDAO;
+import com.shownf.reptile.repository.CommentRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetCommentsDAOBean {
+
+    CommentRepositoryJPA commentRepositoryJPA;
+
+    @Autowired
+    public GetCommentsDAOBean(CommentRepositoryJPA commentRepositoryJPA) {
+        this.commentRepositoryJPA = commentRepositoryJPA;
+    }
+
+    // 게시물에 대한 댓글 객체들 찾기
+    public List<CommentDAO> exec(Long pId){
+        return new ArrayList<>(commentRepositoryJPA.findBypId(pId));
+    }
 }
