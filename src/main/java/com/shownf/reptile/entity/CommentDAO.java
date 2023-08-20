b2ba11c90eaf897b77faea4850642ae07008a1ff
Lee <lsmin476@naver.com>
2023-08-20 23:16:37 +0900

    |SM UPDATE| DAO 댓글 0820

--- src/main/java/com/shownf/reptile/entity/CommentDAO.java
+++ src/main/java/com/shownf/reptile/entity/CommentDAO.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.entity;public class CommentDAO {
+package com.shownf.reptile.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CommentDAO {
+    @Id
+    Long cId;
+    Long pId;
+    String uId;
+    String content;
+    LocalDateTime uploadTime;
 }
