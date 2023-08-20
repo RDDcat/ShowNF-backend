dc161410bb9b9129e7a417a87f8ee65d07363575
Lee <lsmin476@naver.com>
2023-08-21 01:25:42 +0900

    |SM UPDATE| DTO 댓글 객체 요청 DTO 0821

--- src/main/java/com/shownf/reptile/DTO/RequestCommentsDTO.java
+++ src/main/java/com/shownf/reptile/DTO/RequestCommentsDTO.java
@@ -1,2 +1,14 @@
-package com.shownf.reptile.DTO;public class RequestCommentsDTO {
+package com.shownf.reptile.DTO;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class RequestCommentsDTO {
+    Long cId;
+    Long pId;
+    String uId;
+    String content;
+    LocalDateTime uploadTime;
 }
