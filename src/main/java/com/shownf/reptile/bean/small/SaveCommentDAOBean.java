aab99c0f30ca9a4254d59a0b72f1e995101351a8
Lee <lsmin476@naver.com>
2023-08-20 23:17:33 +0900

    |SM UPDATE| small Bean 댓글 repository 저장하기 0820

--- src/main/java/com/shownf/reptile/bean/small/SaveCommentDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveCommentDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class SaveCommentDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentDAO;
+import com.shownf.reptile.repository.CommentRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveCommentDAOBean {
+
+    CommentRepositoryJPA commentRepositoryJPA;
+
+    @Autowired
+    public SaveCommentDAOBean(CommentRepositoryJPA commentRepositoryJPA) {
+        this.commentRepositoryJPA = commentRepositoryJPA;
+    }
+
+    // 댓글 저장
+    public void exec(CommentDAO commentDAO){
+        commentRepositoryJPA.save(commentDAO);
+    }
 }
