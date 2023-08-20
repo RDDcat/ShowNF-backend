11eb9fc6735faaa8dec59fd4dba93e4d85ffabe2
Lee <lsmin476@naver.com>
2023-08-20 17:17:15 +0900

    |SM UPDATE| small Bean 유니크 pId 생성 0820

--- src/main/java/com/shownf/reptile/bean/small/CreateUniqueIdBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateUniqueIdBean.java
@@ -1,2 +1,15 @@
-package com.shownf.reptile.bean.small;public class CreateIdBean {
+package com.shownf.reptile.bean.small;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateUniqueIdBean {
+    // 해시로 Unique한 데이터 값 받기
+    public long exec(){
+        // 아이디 객체 생성
+        CreateUniqueIdBean createUniqueIdBean = new CreateUniqueIdBean();
+
+        // 아이디 객체의 hash값으로 유니크한 아이디값 생성 후 반환
+        return createUniqueIdBean.hashCode();
+    }
 }
