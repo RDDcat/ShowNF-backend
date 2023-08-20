aa07744d6e139379b373a07e85312dfc85a0b989
Lee <lsmin476@naver.com>
2023-08-20 23:19:19 +0900

    |SM UPDATE| Controller 댓글 저장 기능 컨트롤러 0820

--- src/main/java/com/shownf/reptile/controller/CommentController.java
+++ src/main/java/com/shownf/reptile/controller/CommentController.java
@@ -1,2 +1,43 @@
-package com.shownf.reptile.controller;public class CommentController {
+package com.shownf.reptile.controller;
+
+import com.shownf.reptile.DTO.RequestCommentSaveDTO;
+import com.shownf.reptile.DTO.RequestPostSaveDTO;
+import com.shownf.reptile.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@CrossOrigin("*")
+public class CommentController {
+
+    CommentService commentService;
+
+    @Autowired
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    // 댓글 저장
+    @PostMapping("comment")
+    public ResponseEntity<Map<String, Object>> savePost(@RequestBody RequestCommentSaveDTO requestCommentSaveDTO){
+        Long cId = commentService.saveComment(requestCommentSaveDTO);
+
+        // HTTP 상태 변환
+        HttpStatus httpStatus = (cId != null) ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        // 메시지와 id 값 json 데이터로 반환
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("message", (cId != null) ? "Save Success" : "Save Fail");
+        requestMap.put("cId", cId);
+
+        return ResponseEntity.status(httpStatus).body(requestMap);
+    }
 }
