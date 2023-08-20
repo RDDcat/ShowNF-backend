7261d6307270041582ac0c372af3a85c955abf07
Lee <lsmin476@naver.com>
2023-08-21 01:25:16 +0900

    |SM UPDATE| Repository 댓글 레포지토리 JPA pId로 찾기 기능 0821

--- src/main/java/com/shownf/reptile/repository/CommentRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/CommentRepositoryJPA.java
@@ -3,5 +3,8 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.entity.CommentDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepositoryJPA extends JpaRepository<CommentDAO, Long> {
+    List<CommentDAO> findBypId(Long pId);
 }
