0b31fffc4754fc27b081ab65778f10296df4fd3f
Lee <lsmin476@naver.com>
2023-08-20 17:26:16 +0900

    |SM UPDATE| Service 게시물 저장 기능 서비스 0820

--- src/main/java/com/shownf/reptile/service/PostService.java
+++ src/main/java/com/shownf/reptile/service/PostService.java
@@ -1,7 +1,9 @@
 package com.shownf.reptile.service;
 
 import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.DTO.RequestPostSaveDTO;
 import com.shownf.reptile.bean.GetPostBean;
+import com.shownf.reptile.bean.SavePostBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +11,12 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     GetPostBean getPostBean;
+    SavePostBean savePostBean;
 
     @Autowired
-    public PostService(GetPostBean getPostBean) {
+    public PostService(GetPostBean getPostBean, SavePostBean savePostBean) {
         this.getPostBean = getPostBean;
+        this.savePostBean = savePostBean;
     }
 
     // 게시물 조회
@@ -20,8 +24,10 @@ public class PostService {
         return getPostBean.exec(pId, uId);
     }
 
-
     // 게시물 저장
+    public long savePostDAO(RequestPostSaveDTO requestPostSaveDTO){
+        return  savePostBean.exec(requestPostSaveDTO);
+    }
 
     // 게시물 수정
 
