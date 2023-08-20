53be8e14ec60db471cb3d8c643979a48119b1e00
Lee <lsmin476@naver.com>
2023-08-20 17:23:59 +0900

    |SM UPDATE| small Bean 게시물 DAO 객체 생성하기 0820

--- src/main/java/com/shownf/reptile/bean/small/CreatePostDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreatePostDAOBean.java
@@ -1,2 +1,33 @@
-package com.shownf.reptile.bean.small;public class CreatePostDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestPostSaveDTO;
+import com.shownf.reptile.entity.PostDAO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreatePostDAOBean {
+
+    // 게시물 저장시 DAO 생성
+    public PostDAO exec(long pId, RequestPostSaveDTO requestPostSaveDTO){
+
+        // 유저 아이디
+        String uId = requestPostSaveDTO.getUId();
+
+        // 제목
+        String title = requestPostSaveDTO.getTitle();
+
+        // 내용
+        String content = requestPostSaveDTO.getContent();
+
+        // 카테고리
+        String category = requestPostSaveDTO.getCategory();
+
+        // 시간
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        // DTO 반환
+        return new PostDAO(pId, uId, title, content, category, localDateTime);
+    }
 }
