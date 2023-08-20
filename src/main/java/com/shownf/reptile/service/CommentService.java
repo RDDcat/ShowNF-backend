2de20256cbbc499c6e697b3dc733f1c0c05e702f
Lee <lsmin476@naver.com>
2023-08-20 23:19:05 +0900

    |SM UPDATE| Service 댓글 저장 기능 0820

--- src/main/java/com/shownf/reptile/service/CommentService.java
+++ src/main/java/com/shownf/reptile/service/CommentService.java
@@ -1,2 +1,30 @@
-package com.shownf.reptile.service;public class CommentService {
+package com.shownf.reptile.service;
+
+import com.shownf.reptile.DTO.RequestCommentSaveDTO;
+import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.DTO.RequestPostSaveDTO;
+import com.shownf.reptile.bean.SaveCommentBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+    SaveCommentBean saveCommentBean;
+
+    @Autowired
+    public CommentService(SaveCommentBean saveCommentBean) {
+        this.saveCommentBean = saveCommentBean;
+    }
+
+    // 댓글 전체 조회
+
+    // 댓글 저장
+    public Long saveComment(RequestCommentSaveDTO requestCommentSaveDTO){
+        return saveCommentBean.exec(requestCommentSaveDTO);
+    }
+
+    // 댓글 수정
+
+    // 댓글 삭제
 }
