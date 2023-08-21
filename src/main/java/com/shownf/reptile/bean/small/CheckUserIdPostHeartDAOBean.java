84a36036c2174441746ad7d061313b2fda2f21b7
Lee <lsmin476@naver.com>
2023-08-21 19:52:40 +0900

    |SM UPDATE| small Bean 게시물 좋아요 uId 확인하기 0821

--- src/main/java/com/shownf/reptile/bean/small/CheckUserIdPostHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckUserIdPostHeartDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckUserIdPostHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestPostHeartDeleteDTO;
+import com.shownf.reptile.entity.PostHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckUserIdPostHeartDAOBean {
+
+    // 좋아요 uId 판별
+    public boolean exec(PostHeartDAO postHeartDAO, RequestPostHeartDeleteDTO requestPostHeartDeleteDTO){
+        if (postHeartDAO.getUId().equals(requestPostHeartDeleteDTO.getUId()))
+            return true;
+        return false;
+    }
 }
