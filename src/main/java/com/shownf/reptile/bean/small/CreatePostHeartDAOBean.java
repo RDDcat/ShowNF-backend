a614a0afa8ce7f639d9749d2d357a49d9d82afe1
Lee <lsmin476@naver.com>
2023-08-21 17:46:23 +0900

    |SM UPDATE| small Bean 게시물 좋아요 DAO 객체 생성하기 0821

--- src/main/java/com/shownf/reptile/bean/small/CreatePostHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreatePostHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class CreatePostHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestPostHeartDTO;
+import com.shownf.reptile.entity.PostHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreatePostHeartDAOBean {
+
+    // 좋아요 저장시 DAO 생성
+    public PostHeartDAO exec(Long hId, RequestPostHeartDTO requestPostHeartDTO){
+
+        // 게시물 아이디
+        Long pId = requestPostHeartDTO.getPId();
+
+        // 유저 아이디
+        String uId = requestPostHeartDTO.getUId();
+
+        // DAO 반환
+        return new PostHeartDAO(hId, pId, uId);
+    }
 }
