6b104d7b56a77a6c8f9bff6e3334bfde96feef64
Lee <lsmin476@naver.com>
2023-08-22 00:27:05 +0900

    |SM UPDATE| small Bean 게시물 댓글 repository 삭제하기 0822

--- src/main/java/com/shownf/reptile/bean/small/DeleteCommentDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/DeleteCommentDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class DeleteCommentDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentDAO;
+import com.shownf.reptile.repository.CommentRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCommentDAOBean {
+
+    CommentRepositoryJPA commentRepositoryJPA;
+
+    @Autowired
+    public DeleteCommentDAOBean(CommentRepositoryJPA commentRepositoryJPA) {
+        this.commentRepositoryJPA = commentRepositoryJPA;
+    }
+
+    // 댓글 삭제
+    public void exec(CommentDAO commentDAO){
+        commentRepositoryJPA.delete(commentDAO);
+    }
 }
