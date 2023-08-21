3a4203c5e414ab055b760e0f8d66678623518a31
Lee <lsmin476@naver.com>
2023-08-22 00:27:32 +0900

    |SM UPDATE| small Bean 게시물 댓글 갯수 감소에 따른 수정 0822

--- src/main/java/com/shownf/reptile/bean/small/UpdatePostCommentCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdatePostCommentCountDAOBean.java
@@ -16,6 +16,7 @@ public class UpdatePostCommentCountDAOBean {
         this.postRepositoryJPA = postRepositoryJPA;
     }
 
+    // 게시물 댓글 갯수 추가
     public PostDAO exec(CommentDAO commentDAO){
 
         // pId 가져오기
@@ -26,6 +27,22 @@ public class UpdatePostCommentCountDAOBean {
 
         // 게시물 댓글 수 1 증가
         postDAO.setCommentCount(postDAO.getCommentCount() + 1);
+
+        // 게시물 반환
+        return postDAO;
+    }
+
+    // 게시물 댓글 갯수 감소
+    public PostDAO exec(Long cId, CommentDAO commentDAO){
+
+        // pId 가져오기
+        Long pId = commentDAO.getPId();
+
+        // pId 로 게시물 찾기
+        PostDAO postDAO = postRepositoryJPA.findById(pId).get();
+
+        // 게시물 댓글 수 1 감소
+        postDAO.setCommentCount(postDAO.getCommentCount() - 1);
 
         // 게시물 반환
         return postDAO;
