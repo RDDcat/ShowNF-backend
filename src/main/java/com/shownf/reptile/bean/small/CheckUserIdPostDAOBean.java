a603bd71dae09928738d72ef781a7717f5bc8f6d
Lee <lsmin476@naver.com>
2023-08-22 00:26:16 +0900

    |SM UPDATE| small Bean 게시물 댓글 uId 확인하기 0822

--- src/main/java/com/shownf/reptile/bean/small/CheckUserIdPostDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckUserIdPostDAOBean.java
@@ -1,6 +1,10 @@
 package com.shownf.reptile.bean.small;
 
+import com.shownf.reptile.DTO.RequestCommentDeleteDTO;
+import com.shownf.reptile.DTO.RequestPostHeartDeleteDTO;
+import com.shownf.reptile.entity.CommentDAO;
 import com.shownf.reptile.entity.PostDAO;
+import com.shownf.reptile.entity.PostHeartDAO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +13,20 @@ public class CheckUserIdPostDAOBean {
     // 게시물 uId 판별
     public boolean exec(PostDAO postDAO, String uId){
         if(postDAO.getUId().equals(uId))
+            return true;
+        return false;
+    }
+
+    // 좋아요 uId 판별
+    public boolean exec(PostHeartDAO postHeartDAO, RequestPostHeartDeleteDTO requestPostHeartDeleteDTO){
+        if (postHeartDAO.getUId().equals(requestPostHeartDeleteDTO.getUId()))
+            return true;
+        return false;
+    }
+
+    // 댓글 uId 판별
+    public boolean exec(CommentDAO commentDAO, RequestCommentDeleteDTO requestCommentDeleteDTO){
+        if (commentDAO.getUId().equals(requestCommentDeleteDTO.getUId()))
             return true;
         return false;
     }
