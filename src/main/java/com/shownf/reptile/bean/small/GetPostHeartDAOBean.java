f48d9caaa37ee5271173e59637547f0715823b55
Lee <lsmin476@naver.com>
2023-08-21 19:51:27 +0900

    |SM UPDATE| smallbean 게시물 좋아요 DAO 가져오기 0821

--- src/main/java/com/shownf/reptile/bean/small/GetPostHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetPostHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.PostHeartDAO;
+import com.shownf.reptile.repository.PostHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetPostHeartDAOBean {
+
+    PostHeartRepositoryJPA postHeartRepositoryJPA;
+
+    @Autowired
+    public GetPostHeartDAOBean(PostHeartRepositoryJPA postHeartRepositoryJPA) {
+        this.postHeartRepositoryJPA = postHeartRepositoryJPA;
+    }
+
+    // 좋아요 객체 찾기
+    public PostHeartDAO exec(Long hId){
+        return postHeartRepositoryJPA.findById(hId).get();
+    }
 }
