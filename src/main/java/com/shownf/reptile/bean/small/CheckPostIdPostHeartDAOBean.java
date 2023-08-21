c227cd0bc8951429b89900e9044838ee4389606f
Lee <lsmin476@naver.com>
2023-08-21 19:52:29 +0900

    |SM UPDATE| small Bean 게시물 좋아요 pId 확인하기 0821

--- src/main/java/com/shownf/reptile/bean/small/CheckPostIdPostHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckPostIdPostHeartDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckPostIdPostHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestPostHeartDeleteDTO;
+import com.shownf.reptile.entity.PostHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckPostIdPostHeartDAOBean {
+
+    // 좋아요 pId 판별
+    public boolean exec(PostHeartDAO postHeartDAO, RequestPostHeartDeleteDTO requestPostHeartDeleteDTO){
+        if (postHeartDAO.getPId().equals(requestPostHeartDeleteDTO.getPId()))
+            return true;
+        return false;
+    }
 }
