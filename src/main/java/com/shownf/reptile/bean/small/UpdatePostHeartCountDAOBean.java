ab16dd3af4c96eadd0777211049b24f60a7f9efb
Lee <lsmin476@naver.com>
2023-08-21 17:47:37 +0900

    |SM UPDATE| small Bean 게시물 좋아요 갯수 추가에 따른 수정 0821

--- src/main/java/com/shownf/reptile/bean/small/UpdatePostHeartCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdatePostHeartCountDAOBean.java
@@ -1,2 +1,36 @@
-package com.shownf.reptile.bean.small;public class UpdatePostHeartCountDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.PostDAO;
+import com.shownf.reptile.entity.PostHeartDAO;
+import com.shownf.reptile.repository.PostRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdatePostHeartCountDAOBean {
+
+    PostRepositoryJPA postRepositoryJPA;
+    SavePostDAOBean savePostDAOBean;
+
+    @Autowired
+    public UpdatePostHeartCountDAOBean(PostRepositoryJPA postRepositoryJPA, SavePostDAOBean savePostDAOBean) {
+        this.postRepositoryJPA = postRepositoryJPA;
+        this.savePostDAOBean = savePostDAOBean;
+    }
+
+    // 게시물 좋아요 갯수 추가
+    public PostDAO exec(PostHeartDAO postHeartDAO){
+
+        // pId 가져오기
+        Long pId = postHeartDAO.getPId();
+
+        // pId 로 게시물 찾기
+        PostDAO postDAO = postRepositoryJPA.findById(pId).get();
+
+        // 게시물 좋아요 수 1 증가
+        postDAO.setHeartCount(postDAO.getHeartCount() + 1);
+
+        // 게시물 반환
+        return postDAO;
+    }
 }
