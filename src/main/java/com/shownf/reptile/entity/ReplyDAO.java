ed9e68366c8885f85c10e2a9b205dc22b38e9719
Lee <lsmin476@naver.com>
2023-08-22 20:20:46 +0900

    |SM ADD| 대댓글 추가 관련 클래스 생성 0822

---
+++ src/main/java/com/shownf/reptile/entity/ReplyDAO.java
@@ -0,0 +1,21 @@
+package com.shownf.reptile.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ReplyCommentDAO {
+    @Id
+    Long rCId;
+    Long cId;
+    String uId;
+}
