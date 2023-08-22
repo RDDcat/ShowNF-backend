9575834a0a5d961330f062766934b5573fbb7754
Lee <lsmin476@naver.com>
2023-08-22 17:36:40 +0900

    |SM UPDATE| small Bean 댓글 좋아요 DAO 객체 생성하기 0822

--- src/main/java/com/shownf/reptile/bean/small/CreateCommentHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateCommentHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class createCommentHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestCommentHeartSaveDTO;
+import com.shownf.reptile.entity.CommentHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreateCommentHeartDAOBean {
+
+    // 댓글 좋아요 저장시 DAO 생성
+    public CommentHeartDAO exec(Long cHid, RequestCommentHeartSaveDTO requestCommentHeartSaveDTO){
+
+        // 댓글 아이디
+        Long cId = requestCommentHeartSaveDTO.getCId();
+
+        // 유저 아이디
+        String uId = requestCommentHeartSaveDTO.getUId();
+
+        // DAO 반환
+        return new CommentHeartDAO(cHid, cId, uId);
+    }
 }
