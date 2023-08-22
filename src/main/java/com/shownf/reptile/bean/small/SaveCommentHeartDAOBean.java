4493503f0d9b801546159b5c21c8e1fc386c17f9
Lee <lsmin476@naver.com>
2023-08-22 17:37:31 +0900

    |SM UPDATE| small Bean 댓글 좋아요 repository 저장하기 0822

--- src/main/java/com/shownf/reptile/bean/small/SaveCommentHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveCommentHeartDAOBean.java
@@ -1,2 +1,20 @@
-package com.shownf.reptile.bean.small;public class SaveCommentHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.repository.CommentHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveCommentHeartDAOBean {
+    CommentHeartRepositoryJPA commentHeartRepositoryJPA;
+
+    @Autowired
+    public SaveCommentHeartDAOBean(CommentHeartRepositoryJPA commentHeartRepositoryJPA) {
+        this.commentHeartRepositoryJPA = commentHeartRepositoryJPA;
+    }
+
+    public void exec(CommentHeartDAO commentHeartDAO){
+        commentHeartRepositoryJPA.save(commentHeartDAO);
+    }
 }
