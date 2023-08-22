efbf69ad750e1cd2d00e16e3e6b77bda54f183fb
Lee <lsmin476@naver.com>
2023-08-22 20:25:22 +0900

    |SM UPDATE| small Bean 댓글 대댓글 갯수 추가에 따른 수정 0822

--- src/main/java/com/shownf/reptile/bean/small/UpdateCommentReplyCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateCommentReplyCountDAOBean.java
@@ -1,2 +1,35 @@
-package com.shownf.reptile.bean.small;public class UpdateCommentReplyCountDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentDAO;
+import com.shownf.reptile.entity.PostDAO;
+import com.shownf.reptile.entity.ReplyDAO;
+import com.shownf.reptile.repository.CommentRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCommentReplyCountDAOBean {
+
+    CommentRepositoryJPA commentRepositoryJPA;
+
+    @Autowired
+    public UpdateCommentReplyCountDAOBean(CommentRepositoryJPA commentRepositoryJPA) {
+        this.commentRepositoryJPA = commentRepositoryJPA;
+    }
+
+    // 댓글 대댓글 갯수 추가
+    public CommentDAO exec(ReplyDAO replyDAO){
+
+        // cId 가져오기
+        Long cId = replyDAO.getCId();
+
+        // cId 로 댓글 찾기
+        CommentDAO commentDAO = commentRepositoryJPA.findById(cId).get();
+
+        // 댓글 대댓글 수 1 증가
+        commentDAO.setReplyCount(commentDAO.getReplyCount() + 1);
+
+        // 댓글 반환
+        return commentDAO;
+    }
 }
