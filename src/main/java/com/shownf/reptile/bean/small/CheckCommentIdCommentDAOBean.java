12387d05f5347190710c6820c7155d74603a0fa5
Lee <lsmin476@naver.com>
2023-08-22 18:10:09 +0900

    |SM UPDATE| small Bean 댓글 좋아요 cId 확인하기 0822

--- src/main/java/com/shownf/reptile/bean/small/CheckCommentIdCommentDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckCommentIdCommentDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckCommentIdCommentDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestCommentHeartDeleteDTO;
+import com.shownf.reptile.entity.CommentHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckCommentIdCommentDAOBean {
+
+    // 좋아요 cId 판별
+    public boolean exec(CommentHeartDAO commentHeartDAO, RequestCommentHeartDeleteDTO requestCommentHeartDeleteDTO){
+        if (commentHeartDAO.getCId().equals(requestCommentHeartDeleteDTO.getCId()))
+            return true;
+        return false;
+    }
 }
