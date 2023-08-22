db3267e9f5a3ba33c5a5ff139aadf3b6b7c78300
Lee <lsmin476@naver.com>
2023-08-22 22:16:22 +0900

    |SM UPDATE| small Bean 대댓글 uId 확인하기 0822

--- src/main/java/com/shownf/reptile/bean/small/CheckUserIdCommentDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckUserIdCommentDAOBean.java
@@ -1,7 +1,9 @@
 package com.shownf.reptile.bean.small;
 
 import com.shownf.reptile.DTO.RequestCommentHeartDeleteDTO;
+import com.shownf.reptile.DTO.RequestReplyDeleteDTO;
 import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.entity.ReplyDAO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +12,13 @@ public class CheckUserIdCommentDAOBean {
     // 댓글 좋아요 uId 판별
     public boolean exec(CommentHeartDAO commentHeartDAO, RequestCommentHeartDeleteDTO requestCommentHeartDeleteDTO){
         if (commentHeartDAO.getUId().equals(requestCommentHeartDeleteDTO.getUId()))
+            return true;
+        return false;
+    }
+
+    // 대댓글 uId 판별
+    public boolean exec(ReplyDAO replyDAO, RequestReplyDeleteDTO requestReplyDeleteDTO){
+        if (replyDAO.getUId().equals(requestReplyDeleteDTO.getUId()))
             return true;
         return false;
     }
