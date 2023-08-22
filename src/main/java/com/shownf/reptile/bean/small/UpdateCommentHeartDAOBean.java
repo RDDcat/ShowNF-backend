474ebef3f65feeef478d74282e5ed9b20059775a
Lee <lsmin476@naver.com>
2023-08-22 17:37:53 +0900

    |SM UPDATE| small Bean 댓글 좋아요 갯수 추가에 따른 수정 0822

--- src/main/java/com/shownf/reptile/bean/small/UpdateCommentHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateCommentHeartDAOBean.java
@@ -1,2 +1,35 @@
-package com.shownf.reptile.bean.small;public class UpdateCommentHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentDAO;
+import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.entity.PostDAO;
+import com.shownf.reptile.repository.CommentRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateCommentHeartDAOBean {
+
+    CommentRepositoryJPA commentRepositoryJPA;
+
+    @Autowired
+    public UpdateCommentHeartDAOBean(CommentRepositoryJPA commentRepositoryJPA) {
+        this.commentRepositoryJPA = commentRepositoryJPA;
+    }
+
+    // 댓글 좋아요 갯수 추가
+    public CommentDAO exec(CommentHeartDAO commentHeartDAO){
+
+        // cId 가져오기
+        Long cId = commentHeartDAO.getCId();
+
+        // cId 로 게시물 찾기
+        CommentDAO commentDAO = commentRepositoryJPA.findById(cId).get();
+
+        // 댓글 좋아요 수 1 증가
+        commentDAO.setHeartCount(commentDAO.getHeartCount() + 1);
+
+        // 댓글 반환
+        return commentDAO;
+    }
 }
