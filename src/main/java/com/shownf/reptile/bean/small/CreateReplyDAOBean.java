f885f804f5c9407da63c350cd6a0353f4d0db4a7
Lee <lsmin476@naver.com>
2023-08-22 20:22:56 +0900

    |SM UPDATE| small Bean 대댓글 DAO 객체 생성하기 0822

--- src/main/java/com/shownf/reptile/bean/small/CreateReplyDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateReplyDAOBean.java
@@ -1,2 +1,30 @@
-package com.shownf.reptile.bean.small;public class CreateReplyDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestReplySaveDTO;
+import com.shownf.reptile.entity.ReplyDAO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreateReplyDAOBean {
+
+    // 대댓글 저장시 DAO 생성
+    public ReplyDAO exec(Long rId, RequestReplySaveDTO requestReplySaveDTO){
+
+        // 댓글 아이디
+        Long cId = requestReplySaveDTO.getCId();
+
+        // 유저 아이디
+        String uId = requestReplySaveDTO.getUId();
+
+        // 내용
+        String content = requestReplySaveDTO.getContent();
+
+        // 시간
+        LocalDateTime uploadTime = LocalDateTime.now();
+
+        // DAO 반환
+        return new ReplyDAO(rId, cId, uId, content, uploadTime);
+    }
 }
