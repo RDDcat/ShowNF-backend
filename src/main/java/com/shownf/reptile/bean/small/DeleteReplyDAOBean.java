6955c504603ced528a60a4b6cdad3df7b6d13790
Lee <lsmin476@naver.com>
2023-08-22 22:16:53 +0900

    |SM UPDATE| small Bean 대댓글 repository 삭제하기 0822

--- src/main/java/com/shownf/reptile/bean/small/DeleteReplyDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/DeleteReplyDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class DeleteReplyDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ReplyDAO;
+import com.shownf.reptile.repository.ReplyRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteReplyDAOBean {
+
+    ReplyRepositoryJPA replyRepositoryJPA;
+
+    @Autowired
+    public DeleteReplyDAOBean(ReplyRepositoryJPA replyRepositoryJPA) {
+        this.replyRepositoryJPA = replyRepositoryJPA;
+    }
+
+    // 대댓글 삭제
+    public void exec(ReplyDAO replyDAO){
+        replyRepositoryJPA.delete(replyDAO);
+    }
 }
