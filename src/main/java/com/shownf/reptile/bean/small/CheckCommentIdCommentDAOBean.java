5287e82c1fd9fc10ae8c51aad775b390c2c75475
Lee <lsmin476@naver.com>
2023-08-22 22:16:15 +0900

    |SM UPDATE| small Bean 대댓글 cId 확인하기 0822

--- src/main/java/com/shownf/reptile/bean/small/CheckCommentIdCommentDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckCommentIdCommentDAOBean.java
@@ -1,7 +1,9 @@
 package com.shownf.reptile.bean.small;
 
 import com.shownf.reptile.DTO.RequestCommentHeartDeleteDTO;
+import com.shownf.reptile.DTO.RequestReplyDeleteDTO;
 import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.entity.ReplyDAO;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +12,13 @@ public class CheckCommentIdCommentDAOBean {
     // 좋아요 cId 판별
     public boolean exec(CommentHeartDAO commentHeartDAO, RequestCommentHeartDeleteDTO requestCommentHeartDeleteDTO){
         if (commentHeartDAO.getCId().equals(requestCommentHeartDeleteDTO.getCId()))
+            return true;
+        return false;
+    }
+
+    // 대댓글 cId 판별
+    public boolean exec(ReplyDAO replyDAO, RequestReplyDeleteDTO requestReplyDeleteDTO){
+        if (replyDAO.getCId().equals(requestReplyDeleteDTO.getCId()))
             return true;
         return false;
     }
