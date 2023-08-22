6369ef0fbe30408472af826b2d226233f7c8e3ba
Lee <lsmin476@naver.com>
2023-08-22 18:12:33 +0900

    |SM UPDATE| small Bean 댓글 좋아요 갯수 감소에 따른 수정 0822

--- src/main/java/com/shownf/reptile/bean/small/UpdateCommentHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateCommentHeartDAOBean.java
@@ -32,4 +32,20 @@ public class UpdateCommentHeartDAOBean {
         // 댓글 반환
         return commentDAO;
     }
+
+    // 댓글 좋아요 갯수 감소
+    public CommentDAO exec(Long cHid, CommentHeartDAO commentHeartDAO){
+
+        // cId 가져오기
+        Long cId = commentHeartDAO.getCId();
+
+        // cId 로 게시물 찾기
+        CommentDAO commentDAO = commentRepositoryJPA.findById(cId).get();
+
+        // 댓글 좋아요 수 1 증가
+        commentDAO.setHeartCount(commentDAO.getHeartCount() - 1);
+
+        // 댓글 반환
+        return commentDAO;
+    }
 }
