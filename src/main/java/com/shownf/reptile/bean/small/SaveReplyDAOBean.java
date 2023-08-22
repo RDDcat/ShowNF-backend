2126e3146712b1e08a07555fcccf2b4334e63b8d
Lee <lsmin476@naver.com>
2023-08-22 20:24:08 +0900

    |SM UPDATE| small Bean 대댓글 repository 저장하기 0822

--- src/main/java/com/shownf/reptile/bean/small/SaveReplyDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveReplyDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class SaveReplyDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ReplyDAO;
+import com.shownf.reptile.repository.ReplyRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveReplyDAOBean {
+
+    ReplyRepositoryJPA replyRepositoryJPA;
+
+    @Autowired
+    public SaveReplyDAOBean(ReplyRepositoryJPA replyRepositoryJPA) {
+        this.replyRepositoryJPA = replyRepositoryJPA;
+    }
+
+    // 대댓글 저장
+    public void exec(ReplyDAO replyDAO){
+        replyRepositoryJPA.save(replyDAO);
+    }
 }
