956dfa79277e37109fd1aeba13ba8aa92f338c48
Lee <lsmin476@naver.com>
2023-08-22 18:12:15 +0900

    |SM UPDATE| small Bean 댓글 좋아요 repository 삭제하기 0822

--- src/main/java/com/shownf/reptile/bean/small/DeleteCommentHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/DeleteCommentHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class DeleteCommentHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.repository.CommentHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteCommentHeartDAOBean {
+
+    CommentHeartRepositoryJPA commentHeartRepositoryJPA;
+
+    @Autowired
+    public DeleteCommentHeartDAOBean(CommentHeartRepositoryJPA commentHeartRepositoryJPA) {
+        this.commentHeartRepositoryJPA = commentHeartRepositoryJPA;
+    }
+
+    // 댓글 좋아요 삭제
+    public void exec(CommentHeartDAO commentHeartDAO){
+        commentHeartRepositoryJPA.delete(commentHeartDAO);
+    }
 }
