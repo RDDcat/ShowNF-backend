946bf8f18057d63b0e914b589ba6042661bc76d8
Lee <lsmin476@naver.com>
2023-08-22 18:08:54 +0900

    |SM UPDATE| small Bean 댓글 좋아요 DAO 가져오기 0822

--- src/main/java/com/shownf/reptile/bean/small/GetCommentHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetCommentHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetCommentHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.repository.CommentHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetCommentHeartDAOBean {
+
+    CommentHeartRepositoryJPA commentHeartRepositoryJPA;
+
+    @Autowired
+    public GetCommentHeartDAOBean(CommentHeartRepositoryJPA commentHeartRepositoryJPA) {
+        this.commentHeartRepositoryJPA = commentHeartRepositoryJPA;
+    }
+
+    // 댓글 좋아요 아이디로 삭제할 댓글 찾기
+    public CommentHeartDAO exec(Long cHId){
+        return commentHeartRepositoryJPA.findById(cHId).get();
+    }
 }
