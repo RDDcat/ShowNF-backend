c78349d068d4400869b281359f601e2477eb5e13
Lee <lsmin476@naver.com>
2023-08-23 23:58:23 +0900

    |SM UPDATE| small Bean 대댓글 좋아요 rId 확인하기 0823

--- src/main/java/com/shownf/reptile/bean/small/CheckReplyIdReplyDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckReplyIdReplyDAOBean.java
@@ -1,2 +1,18 @@
-package com.shownf.reptile.bean.small;public class CheckReplyIdReplyDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestCommentHeartDeleteDTO;
+import com.shownf.reptile.DTO.RequestReplyHeartDeleteDTO;
+import com.shownf.reptile.entity.CommentHeartDAO;
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckReplyIdReplyDAOBean {
+
+    // 대댓글 좋아요 rId 판별
+    public boolean exec(ReplyHeartDAO replyHeartDAO, RequestReplyHeartDeleteDTO requestReplyHeartDeleteDTO){
+        if (replyHeartDAO.getRId().equals(requestReplyHeartDeleteDTO.getRId()))
+            return true;
+        return false;
+    }
 }
