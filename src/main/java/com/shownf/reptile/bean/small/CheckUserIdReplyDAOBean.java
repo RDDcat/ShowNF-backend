c50c712f68e2946dcb5050dde190f2748ddc56c1
Lee <lsmin476@naver.com>
2023-08-23 23:58:30 +0900

    |SM UPDATE| small Bean 대댓글 좋아요 uId 확인하기 0823

--- src/main/java/com/shownf/reptile/bean/small/CheckUserIdReplyDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckUserIdReplyDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckUserIdReplyDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestReplyHeartDeleteDTO;
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckUserIdReplyDAOBean {
+
+    // 대댓글 좋아요 uId 판별
+    public boolean exec(ReplyHeartDAO replyHeartDAO, RequestReplyHeartDeleteDTO requestReplyHeartDeleteDTO){
+        if (replyHeartDAO.getUId().equals(requestReplyHeartDeleteDTO.getUId()))
+            return true;
+        return false;
+    }
 }
