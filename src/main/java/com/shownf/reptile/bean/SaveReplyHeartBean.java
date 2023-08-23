81def42e46e211a2f9d7901d0e27d0b8c43c1fe6
Lee <lsmin476@naver.com>
2023-08-23 21:35:30 +0900

    |SM UPDATE| Bean, Service, Controller 대댓글 좋아요 추가 기능 0823

--- src/main/java/com/shownf/reptile/bean/SaveReplyHeartBean.java
+++ src/main/java/com/shownf/reptile/bean/SaveReplyHeartBean.java
@@ -1,2 +1,49 @@
-package com.shownf.reptile.bean;public class SaveReplyHeartBean {
+package com.shownf.reptile.bean;
+
+import com.shownf.reptile.DTO.RequestReplyHeartSaveDTO;
+import com.shownf.reptile.bean.small.*;
+import com.shownf.reptile.entity.ReplyDAO;
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveReplyHeartBean {
+
+    CreateUniqueIdBean createUniqueIdBean;
+    CreateReplyHeartDAOBean createReplyHeartDAOBean;
+    SaveReplyHeartDAOBean saveReplyHeartDAOBean;
+    UpdateReplyHeartCountDAOBean updateReplyHeartCountDAOBean;
+    SaveReplyDAOBean saveReplyDAOBean;
+
+    @Autowired
+    public SaveReplyHeartBean(CreateUniqueIdBean createUniqueIdBean, CreateReplyHeartDAOBean createReplyHeartDAOBean, SaveReplyHeartDAOBean saveReplyHeartDAOBean, UpdateReplyHeartCountDAOBean updateReplyHeartCountDAOBean, SaveReplyDAOBean saveReplyDAOBean) {
+        this.createUniqueIdBean = createUniqueIdBean;
+        this.createReplyHeartDAOBean = createReplyHeartDAOBean;
+        this.saveReplyHeartDAOBean = saveReplyHeartDAOBean;
+        this.updateReplyHeartCountDAOBean = updateReplyHeartCountDAOBean;
+        this.saveReplyDAOBean = saveReplyDAOBean;
+    }
+
+    // 대댓글 좋아요 저장
+    public Long exec(RequestReplyHeartSaveDTO requestReplyHeartSaveDTO){
+
+        // rHId 생성
+        Long rHid = createUniqueIdBean.exec();
+
+        // DTO 객체 DAO 변환
+        ReplyHeartDAO replyHeartDAO = createReplyHeartDAOBean.exec(rHid, requestReplyHeartSaveDTO);
+
+        // 대댓글 좋아요 저장
+        saveReplyHeartDAOBean.exec(replyHeartDAO);
+
+        // 대댓글 좋아요 갯수 추가
+        ReplyDAO replyDAO = updateReplyHeartCountDAOBean.exec(replyHeartDAO);
+
+        // 대댓글 저장
+        saveReplyDAOBean.exec(replyDAO);
+
+        // rHId 반환
+        return rHid;
+    }
 }
