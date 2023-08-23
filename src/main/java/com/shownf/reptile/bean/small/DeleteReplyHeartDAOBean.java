490fa30e9b9fc8e7b220f14923120502493ec30c
Lee <lsmin476@naver.com>
2023-08-23 23:58:48 +0900

    |SM UPDATE| small Bean 대댓글 좋아요 repository 삭제하기 0823

--- src/main/java/com/shownf/reptile/bean/small/DeleteReplyHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/DeleteReplyHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class DeleteReplyHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import com.shownf.reptile.repository.ReplyHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteReplyHeartDAOBean {
+
+    ReplyHeartRepositoryJPA replyHeartRepositoryJPA;
+
+    @Autowired
+    public DeleteReplyHeartDAOBean(ReplyHeartRepositoryJPA replyHeartRepositoryJPA) {
+        this.replyHeartRepositoryJPA = replyHeartRepositoryJPA;
+    }
+
+    // 대댓글 좋아요 삭제
+    public void exec(ReplyHeartDAO replyHeartDAO){
+        replyHeartRepositoryJPA.delete(replyHeartDAO);
+    }
 }
