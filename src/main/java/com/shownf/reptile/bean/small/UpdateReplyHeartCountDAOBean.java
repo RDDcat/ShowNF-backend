4021e4ac1566e6e945bff236f396f4da6166e3ff
Lee <lsmin476@naver.com>
2023-08-23 21:35:18 +0900

    |SM UPDATE| small Bean 대댓글 좋아요 갯수 추가에 따른 수정 0823

--- src/main/java/com/shownf/reptile/bean/small/UpdateReplyHeartCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateReplyHeartCountDAOBean.java
@@ -1,2 +1,34 @@
-package com.shownf.reptile.bean.small;public class UpdateReplyHeartCountDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ReplyDAO;
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import com.shownf.reptile.repository.ReplyRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateReplyHeartCountDAOBean {
+
+    ReplyRepositoryJPA replyRepositoryJPA;
+
+    @Autowired
+    public UpdateReplyHeartCountDAOBean(ReplyRepositoryJPA replyRepositoryJPA) {
+        this.replyRepositoryJPA = replyRepositoryJPA;
+    }
+
+    // 대댓글 좋아요 갯수 추가
+    public ReplyDAO exec(ReplyHeartDAO replyHeartDAO){
+
+        // rId 가져오기
+        Long rId = replyHeartDAO.getRId();
+
+        // rId 로 댓글 찾기
+        ReplyDAO replyDAO = replyRepositoryJPA.findById(rId).get();
+
+        // 대댓글 좋아요 수 1 증가
+        replyDAO.setHeartCount(replyDAO.getHeartCount() + 1);
+
+        // 대댓글 반환
+        return replyDAO;
+    }
 }
