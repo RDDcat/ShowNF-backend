5b046752622da28825fc80e4323a8d33f121d38e
Lee <lsmin476@naver.com>
2023-08-23 21:34:22 +0900

    |SM UPDATE| small Bean 대댓글 좋아요 DAO 객체 생성하기 0823

--- src/main/java/com/shownf/reptile/bean/small/CreateReplyHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateReplyHeartDAOBean.java
@@ -1,2 +1,26 @@
-package com.shownf.reptile.bean.small;public class CreateReplyHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestReplyHeartSaveDTO;
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreateReplyHeartDAOBean {
+
+    // 대댓글 좋아요 저장시 DAO 생성
+    public ReplyHeartDAO exec(Long rHId, RequestReplyHeartSaveDTO requestReplyHeartSaveDTO){
+
+        // 대댓글 아이디
+        Long rId = requestReplyHeartSaveDTO.getRId();
+
+        // 유저 아이디
+        String uId = requestReplyHeartSaveDTO.getUId();
+
+        // 시간
+        LocalDateTime uploadTime = LocalDateTime.now();
+
+        return new ReplyHeartDAO(rHId, rId, uId, uploadTime);
+    }
 }
