91a65893ac2431a77230761ec7dad81efb7b3ae1
Lee <lsmin476@naver.com>
2023-08-23 23:58:03 +0900

    |SM UPDATE| small Bean 대댓글 좋아요 DAO 가져오기 0823

--- src/main/java/com/shownf/reptile/bean/small/GetReplyHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetReplyHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetReplyHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ReplyHeartDAO;
+import com.shownf.reptile.repository.ReplyHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetReplyHeartDAOBean {
+
+    ReplyHeartRepositoryJPA replyHeartRepositoryJPA;
+
+    @Autowired
+    public GetReplyHeartDAOBean(ReplyHeartRepositoryJPA replyHeartRepositoryJPA) {
+        this.replyHeartRepositoryJPA = replyHeartRepositoryJPA;
+    }
+
+    // 대댓글 좋아요 객체 찾기
+    public ReplyHeartDAO exec(Long rHId){
+        return replyHeartRepositoryJPA.findById(rHId).get();
+    }
 }
