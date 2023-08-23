1611d11ec2a69b474cbd81ae5a6bcc3ee54ed774
Lee <lsmin476@naver.com>
2023-08-23 21:35:30 +0900

    |SM UPDATE| Bean, Service, Controller 대댓글 좋아요 추가 기능 0822

--- src/main/java/com/shownf/reptile/controller/ReplyHeartController.java
+++ src/main/java/com/shownf/reptile/controller/ReplyHeartController.java
@@ -1,2 +1,45 @@
-package com.shownf.reptile.controller;public class ReplyHeartController {
+package com.shownf.reptile.controller;
+
+import com.shownf.reptile.DTO.RequestReplyHeartSaveDTO;
+import com.shownf.reptile.service.ReplyHeartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@CrossOrigin("*")
+public class ReplyHeartController {
+
+    ReplyHeartService replyHeartService;
+
+    @Autowired
+    public ReplyHeartController(ReplyHeartService replyHeartService) {
+        this.replyHeartService = replyHeartService;
+    }
+
+    // 대댓글 좋아요 저장
+    @PostMapping("replyHeart")
+    public ResponseEntity<Map<String, Object>> saveReplyHeart(@RequestBody RequestReplyHeartSaveDTO requestReplyHeartSaveDTO){
+        Long rHId = replyHeartService.saveReplyHeart(requestReplyHeartSaveDTO);
+
+        // HTTP 상태 변환
+        HttpStatus httpStatus = (rHId != null) ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        // 메시지와 id 값 json 데이터로 반환
+        Map<String, Object> requestMap = new HashMap<>();
+        requestMap.put("message", (rHId != null) ? "Save Success" : "Save Fail");
+        requestMap.put("rHId", rHId);
+
+        return ResponseEntity.status(httpStatus).body(requestMap);
+    }
+
+    // 대댓글 좋아요 삭제
 }
--- src/main/java/com/shownf/reptile/service/ReplyHeartService.java
+++ src/main/java/com/shownf/reptile/service/ReplyHeartService.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.service;public class ReplyHeartService {
+package com.shownf.reptile.service;
+
+import com.shownf.reptile.DTO.RequestReplyHeartSaveDTO;
+import com.shownf.reptile.bean.SaveReplyHeartBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReplyHeartService {
+
+    SaveReplyHeartBean saveReplyHeartBean;
+
+    @Autowired
+    public ReplyHeartService(SaveReplyHeartBean saveReplyHeartBean) {
+        this.saveReplyHeartBean = saveReplyHeartBean;
+    }
+
+    // 대댓글 좋아요 저장
+    public Long saveReplyHeart(RequestReplyHeartSaveDTO requestReplyHeartSaveDTO){
+        return saveReplyHeartBean.exec(requestReplyHeartSaveDTO);
+    }
+
+    // 대댓글 좋아요 삭제
 }
