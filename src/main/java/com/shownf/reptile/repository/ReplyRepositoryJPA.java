8433c5b48b2f66a13f5cbb1c23bff21b70b1fec0
Lee <lsmin476@naver.com>
2023-08-24 00:48:23 +0900

    |SM UPDATE| Repository 대댓글 레포지토리 JPA cId로 찾기 기능 0824

--- src/main/java/com/shownf/reptile/repository/ReplyRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/ReplyRepositoryJPA.java
@@ -3,5 +3,8 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.entity.ReplyDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReplyRepositoryJPA extends JpaRepository<ReplyDAO, Long> {
+    List<ReplyDAO> findBycId(Long cId);
 }
