35b12f59f2eb93e3773302b5d1d79215a8d78411
Lee <lsmin476@naver.com>
2023-08-24 00:48:52 +0900

    |SM UPDATE| small Bean 대댓글 cId로 찾기 0824

--- src/main/java/com/shownf/reptile/bean/small/GetReplysDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetReplysDAOBean.java
@@ -1,2 +1,25 @@
-package com.shownf.reptile.bean.small;public class GetReplysDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ReplyDAO;
+import com.shownf.reptile.repository.ReplyRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetReplysDAOBean {
+
+    ReplyRepositoryJPA replyRepositoryJPA;
+
+    @Autowired
+    public GetReplysDAOBean(ReplyRepositoryJPA replyRepositoryJPA) {
+        this.replyRepositoryJPA = replyRepositoryJPA;
+    }
+
+    // 댓글에 해당하는 대댓글 전체 조회
+    public List<ReplyDAO> exec(Long cId){
+        return new ArrayList<>(replyRepositoryJPA.findBycId(cId));
+    }
 }
