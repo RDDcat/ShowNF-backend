6e72102310383924f68ed0d0848da49dde4810ea
Lee <lsmin476@naver.com>
2023-08-24 00:51:04 +0900

    |SM UPDATE| Controller @RestController로 변경 0824

--- src/main/java/com/shownf/reptile/controller/CommentHeartController.java
+++ src/main/java/com/shownf/reptile/controller/CommentHeartController.java
@@ -7,15 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @CrossOrigin("*")
 public class CommentHeartController {
 
--- src/main/java/com/shownf/reptile/controller/PostHeartController.java
+++ src/main/java/com/shownf/reptile/controller/PostHeartController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @CrossOrigin("*")
 public class PostHeartController {
 
