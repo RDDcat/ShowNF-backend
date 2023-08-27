dec2d5478f7aaa77b2d1212c92182c3d3106b41b
Lee <lsmin476@naver.com>
2023-08-28 01:38:21 +0900

    |SM UPDATE| DAO, Repository 이미지 저장 객체 생성

--- src/main/java/com/shownf/reptile/entity/ImageDAO.java
+++ src/main/java/com/shownf/reptile/entity/ImageDAO.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.entity;public class ImageDAO {
+package com.shownf.reptile.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ImageDAO {
+    @Id
+    Long iId;
+    String imageName;
+    String imageUrl;
+    /*byte[] information;*/
+    LocalDateTime uploadTime;
 }
--- src/main/java/com/shownf/reptile/repository/ImageRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/ImageRepositoryJPA.java
@@ -1,2 +1,7 @@
-package com.shownf.reptile.repository;public interface ImageRepositoryJPA {
+package com.shownf.reptile.repository;
+
+import com.shownf.reptile.entity.ImageDAO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepositoryJPA extends JpaRepository<ImageDAO, Long> {
 }
