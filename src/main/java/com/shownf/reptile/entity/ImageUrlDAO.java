2cec5d4324bc87aabd503c605a7f08fbaebcb6eb
Lee <lsmin476@naver.com>
2023-08-28 03:27:24 +0900

    |SM UPDATE| DAO, Repository 이미지 Url 저장 객체 생성

--- src/main/java/com/shownf/reptile/entity/ImageUrlDAO.java
+++ src/main/java/com/shownf/reptile/entity/ImageUrlDAO.java
@@ -1,2 +1,20 @@
-package com.shownf.reptile.entity;public class ImageUrlDAO {
+package com.shownf.reptile.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class ImageUrlDAO {
+    @Id
+    Long iId;
+    String imageUrl;
 }
