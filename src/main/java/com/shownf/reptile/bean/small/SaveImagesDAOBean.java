1b2f65442fcaafd4cb6b39867df5375bff4b36af
Lee <lsmin476@naver.com>
2023-08-28 01:37:44 +0900

    |SM UPDATE| small Bean 이미지 저장 DAO 생성 및 저장

--- src/main/java/com/shownf/reptile/bean/small/SaveImagesDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveImagesDAOBean.java
@@ -1,2 +1,26 @@
-package com.shownf.reptile.bean.small;public class SaveImagesDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ImageDAO;
+import com.shownf.reptile.repository.ImageRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SaveImagesDAOBean {
+
+    ImageRepositoryJPA imageRepositoryJPA;
+
+    @Autowired
+    public SaveImagesDAOBean(ImageRepositoryJPA imageRepositoryJPA) {
+        this.imageRepositoryJPA = imageRepositoryJPA;
+    }
+
+    // 이미지 저장
+    public void exec(List<ImageDAO> imageDAOs){
+        for (ImageDAO imageDAO : imageDAOs){
+                imageRepositoryJPA.save(imageDAO);
+        }
+    }
 }
