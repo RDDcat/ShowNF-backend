d3be0c5d008bdb507fc12f5aa98b6a8314bbe4f7
Lee <lsmin476@naver.com>
2023-08-28 19:21:34 +0900

    |SM UPDATE| small Bean 이미지 repository 저장하기

--- src/main/java/com/shownf/reptile/bean/small/SaveImageDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveImageDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class SaveImageDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ImageDAO;
+import com.shownf.reptile.repository.ImageRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveImageDAOBean {
+
+    ImageRepositoryJPA imageRepositoryJPA;
+
+    @Autowired
+    public SaveImageDAOBean(ImageRepositoryJPA imageRepositoryJPA) {
+        this.imageRepositoryJPA = imageRepositoryJPA;
+    }
+
+    // 이미지 저장
+    public void exec(ImageDAO imageDAO){
+        imageRepositoryJPA.save(imageDAO);
+    }
 }
