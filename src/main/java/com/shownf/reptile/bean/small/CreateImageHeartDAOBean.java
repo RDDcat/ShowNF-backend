6dcecabbd294fe44001ea916d3d29e074c7ea7da
Lee <lsmin476@naver.com>
2023-08-28 19:20:20 +0900

    |SM UPDATE| small Bean 이미지 좋아요 DAO 객체 생성하기

--- src/main/java/com/shownf/reptile/bean/small/CreateImageHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateImageHeartDAOBean.java
@@ -1,2 +1,27 @@
-package com.shownf.reptile.bean.small;public class CreateImageHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestImageHeartSaveDTO;
+import com.shownf.reptile.entity.ImageHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreateImageHeartDAOBean {
+
+    // 이미지 좋아요 저장시 DAO 생성
+    public ImageHeartDAO exec(Long iHId, RequestImageHeartSaveDTO requestImageHeartSaveDTO){
+
+        // 이미지 아이디
+        Long iId = requestImageHeartSaveDTO.getIId();
+
+        // 유저 아이디
+        String uId = requestImageHeartSaveDTO.getUId();
+
+        // 업로드 시간
+        LocalDateTime uploadTime = LocalDateTime.now();
+
+        // DAO 반환
+        return new ImageHeartDAO(iHId, iId, uId, uploadTime);
+    }
 }
