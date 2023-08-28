2a9103a5c079d9501f08fcec4cb9432301c45888
Lee <lsmin476@naver.com>
2023-08-28 20:20:32 +0900

    |SM UPDATE| small Bean 이미지 좋아요 갯수 감소에 따른 수정

--- src/main/java/com/shownf/reptile/bean/small/UpdateImageHeartCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateImageHeartCountDAOBean.java
@@ -31,4 +31,20 @@ public class UpdateImageHeartCountDAOBean {
         // 이미지 반환
         return imageDAO;
     }
+
+    // 이미지 좋아요 갯수 감소
+    public ImageDAO exec(Long iHId, ImageHeartDAO imageHeartDAO){
+
+        // iId 가져오기
+        Long iId = imageHeartDAO.getIId();
+
+        // iId 로 이미지 찾기
+        ImageDAO imageDAO = imageRepositoryJPA.findById(iId).get();
+
+        // 이미지 좋아요 수 1 증가
+        imageDAO.setHeartCount(imageDAO.getHeartCount() - 1);
+
+        // 이미지 반환
+        return imageDAO;
+    }
 }
