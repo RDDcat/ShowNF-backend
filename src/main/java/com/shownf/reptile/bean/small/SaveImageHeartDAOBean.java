3837f939ac9dc5d7d579b589dbe1e57695346ade
Lee <lsmin476@naver.com>
2023-08-28 19:20:47 +0900

    |SM UPDATE| small Bean 이미지 좋아요 repository 저장하기

--- src/main/java/com/shownf/reptile/bean/small/SaveImageHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveImageHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class SaveImageHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ImageHeartDAO;
+import com.shownf.reptile.repository.ImageHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveImageHeartDAOBean {
+
+    ImageHeartRepositoryJPA imageHeartRepositoryJPA;
+
+    @Autowired
+    public SaveImageHeartDAOBean(ImageHeartRepositoryJPA imageHeartRepositoryJPA) {
+        this.imageHeartRepositoryJPA = imageHeartRepositoryJPA;
+    }
+
+    // 이미지 좋아요 저장
+    public void exec(ImageHeartDAO imageHeartDAO){
+        imageHeartRepositoryJPA.save(imageHeartDAO);
+    }
 }
