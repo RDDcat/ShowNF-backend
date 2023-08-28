4c69fe20ca6a4db233d4221d99193f13e25f2060
Lee <lsmin476@naver.com>
2023-08-28 20:19:42 +0900

    |SM UPDATE| small Bean 이미지 좋아요 iId 확인하기

--- src/main/java/com/shownf/reptile/bean/small/CheckImageIdImageDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckImageIdImageDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckImageIdImageDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestImageHeartDeleteDTO;
+import com.shownf.reptile.entity.ImageHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckImageIdImageDAOBean {
+
+    // 좋아요 iId 판별
+    public boolean exec(ImageHeartDAO imageHeartDAO, RequestImageHeartDeleteDTO requestImageHeartDeleteDTO){
+        if (imageHeartDAO.getIId().equals(requestImageHeartDeleteDTO.getIId()))
+            return true;
+        return false;
+    }
 }
