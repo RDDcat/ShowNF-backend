14a7880edb602f9df1d2d3df0091686e6a30087c
Lee <lsmin476@naver.com>
2023-08-28 18:26:05 +0900

    |SM ADD| 이미지 저장 관련 클래스 수정

--- src/main/java/com/shownf/reptile/repository/ImageUrlRepositoryJPA.java
+++
@@ -1,7 +0,0 @@
-package com.shownf.reptile.repository;
-
-import com.shownf.reptile.entity.ImageUrlDAO;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageUrlRepositoryJPA extends JpaRepository<ImageUrlDAO, Long> {
-}
