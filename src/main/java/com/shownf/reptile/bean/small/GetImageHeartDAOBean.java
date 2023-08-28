318d5dc5d646c3a1c4eb9e440341541f5b6de635
Lee <lsmin476@naver.com>
2023-08-28 20:19:10 +0900

    |SM UPDATE| small Bean 이미지 좋아요 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetImageHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetImageHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetImageHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ImageHeartDAO;
+import com.shownf.reptile.repository.ImageHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetImageHeartDAOBean {
+
+    ImageHeartRepositoryJPA imageHeartRepositoryJPA;
+
+    @Autowired
+    public GetImageHeartDAOBean(ImageHeartRepositoryJPA imageHeartRepositoryJPA) {
+        this.imageHeartRepositoryJPA = imageHeartRepositoryJPA;
+    }
+
+    // 이미지 좋아요 아이디로 삭제할 좋아요 찾기
+    public ImageHeartDAO exec(Long iHId){
+        return imageHeartRepositoryJPA.findById(iHId).get();
+    }
 }
