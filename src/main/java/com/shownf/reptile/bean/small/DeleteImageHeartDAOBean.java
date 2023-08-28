05a2ce3ff8c283ab8df188a1c1c301465bd823a8
Lee <lsmin476@naver.com>
2023-08-28 20:20:14 +0900

    |SM UPDATE| small Bean 이미지 좋아요 repository 삭제하기

--- src/main/java/com/shownf/reptile/bean/small/DeleteImageHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/DeleteImageHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class DeleteImageHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.ImageHeartDAO;
+import com.shownf.reptile.repository.ImageHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteImageHeartDAOBean {
+
+    ImageHeartRepositoryJPA imageHeartRepositoryJPA;
+
+    @Autowired
+    public DeleteImageHeartDAOBean(ImageHeartRepositoryJPA imageHeartRepositoryJPA) {
+        this.imageHeartRepositoryJPA = imageHeartRepositoryJPA;
+    }
+
+    // 이미지 좋아요 삭제
+    public void exec(ImageHeartDAO imageHeartDAO){
+        imageHeartRepositoryJPA.delete(imageHeartDAO);
+    }
 }
