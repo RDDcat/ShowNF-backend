ea0099aafbbead1d52e2a35a8b240fdb891d1c51
Lee <lsmin476@naver.com>
2023-08-31 22:18:57 +0900

    |SM UPDATE| small Bean postContent repository 저장하기

--- src/main/java/com/shownf/reptile/bean/small/SavePostContentsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SavePostContentsDAOBean.java
@@ -1,2 +1,25 @@
-package com.shownf.reptile.bean.small;public class SavePostContentsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.PostContentDAO;
+import com.shownf.reptile.repository.PostContentRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SavePostContentsDAOBean {
+
+    PostContentRepositoryJPA postContentRepositoryJPA;
+
+    @Autowired
+    public SavePostContentsDAOBean(PostContentRepositoryJPA postContentRepositoryJPA) {
+        this.postContentRepositoryJPA = postContentRepositoryJPA;
+    }
+
+    // 게시물 내용 저장
+    public void exec(List<PostContentDAO> postContentDAOs){
+        for (PostContentDAO postContentDAO : postContentDAOs)
+            postContentRepositoryJPA.save(postContentDAO);
+    }
 }
