659dec0494865782995d11eab373e71a74e606e6
Lee <lsmin476@naver.com>
2023-09-01 23:22:44 +0900

    |SM UPDATE| small Bean Post DTOs 생성

--- src/main/java/com/shownf/reptile/bean/small/CreatePostsDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreatePostsDTOBean.java
@@ -1,2 +1,45 @@
-package com.shownf.reptile.bean.small;public class CreatePostsDTOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.DTO.RequestPostDTO;
+import com.shownf.reptile.entity.PostDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CreatePostsDTOBean {
+
+    // 게시물 조회시 DTO 생성
+    public Page<RequestPostDTO> exec(Pageable pageable, Page<PostDAO> postDAOs){
+
+        List<RequestPostDTO> requestPostDTOs = new ArrayList<>();
+
+        // DTO 객체에 게시물 정보 넘기기
+        for (PostDAO postDAO: postDAOs) {
+            RequestPostDTO requestPostDTO = new RequestPostDTO();
+
+            // 좋아요 순으로 조회함에 따라 좋아요가 없으면 조회X
+            if (postDAO.getHeartCount() <= 0)
+                continue;
+
+            requestPostDTO.setPostId(postDAO.getPostId());
+            requestPostDTO.setUserId(postDAO.getUserId());
+            requestPostDTO.setTitle(postDAO.getTitle());
+            requestPostDTO.setContent(postDAO.getContent());
+            requestPostDTO.setCategory(postDAO.getCategory());
+            requestPostDTO.setUploadTime(postDAO.getUploadTime());
+            requestPostDTO.setHeartCount(postDAO.getHeartCount());
+            requestPostDTO.setCommentCount(postDAO.getCommentCount());
+            requestPostDTO.setViewCount(postDAO.getViewCount());
+
+            requestPostDTOs.add(requestPostDTO);
+        }
+
+        // List 구조를 Page 구조로 변경 후 반환
+        return new PageImpl<>(requestPostDTOs, pageable, postDAOs.getTotalElements());
+    }
 }
