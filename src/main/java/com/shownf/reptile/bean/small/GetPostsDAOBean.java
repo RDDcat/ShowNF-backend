bd01a640aabe931566808d33a01a189c00737369
Lee <lsmin476@naver.com>
2023-09-01 23:21:54 +0900

    |SM UPDATE| small Bean post Page로 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetPostsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetPostsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.entity.PostDAO;
+import com.shownf.reptile.repository.PostRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetPostsDAOBean {
+
+    PostRepositoryJPA postRepositoryJPA;
+
+    @Autowired
+    public GetPostsDAOBean(PostRepositoryJPA postRepositoryJPA) {
+        this.postRepositoryJPA = postRepositoryJPA;
+    }
+
+    // 게시물 찾기
+    public Page<PostDAO> exec(Pageable pageable){
+        return postRepositoryJPA.findAll(pageable);
+    }
 }
