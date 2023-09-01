5a8a4e1e5b93a057009ae7f4a398e6f24acec2e6
Lee <lsmin476@naver.com>
2023-09-01 23:23:13 +0900

    |SM UPDATE| Repository Pageable을 위한 메서드 생성

--- src/main/java/com/shownf/reptile/repository/PostRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/PostRepositoryJPA.java
@@ -1,7 +1,11 @@
 package com.shownf.reptile.repository;
 
 import com.shownf.reptile.entity.PostDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface PostRepositoryJPA extends JpaRepository<PostDAO, Long> {
+    Page<PostDAO> findAll(Pageable pageable);
 }
