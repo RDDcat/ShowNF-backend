ca9b7614f37ed36181e2be6f86393214e08455ad
Lee <lsmin476@naver.com>
2023-09-03 18:07:43 +0900

    |SM UPDATE| Post 카테고리별로 조회에 따른 init 수정

--- src/main/java/com/shownf/reptile/InitDB.java
+++ src/main/java/com/shownf/reptile/InitDB.java
@@ -1,5 +1,6 @@
 package com.shownf.reptile;
 
+import com.shownf.reptile.Model.Enum.Category;
 import com.shownf.reptile.Model.entity.*;
 import com.shownf.reptile.repository.*;
 import lombok.RequiredArgsConstructor;
@@ -34,16 +35,16 @@ public class InitDB {
         private final ImageHeartRepositoryJPA imageHeartRepositoryJPA;
 
         public void dbInit(){
-            PostDAO postDAO1 = new PostDAO(0, "코딩좀비", "테스트 제목1", "[{imageUrl=http://localhost:8080/test1.jpg, content=content1}, {imageUrl=http://localhost:8080/test2.jpg, content=content2}]", "도마뱀", LocalDateTime.now(), 3, 1, 0);
-            PostDAO postDAO2 = new PostDAO(1, "괴물", "테스트 제목2", "[{imageUrl=http://localhost:8080/test3.jpg, content=content3}, {imageUrl=http://localhost:8080/test4.jpg, content=content4}]", "뱀", LocalDateTime.now(), 2, 6, 0);
-            PostDAO postDAO3 = new PostDAO(2, "카페인중독", "테스트 제목3", "[{imageUrl=http://localhost:8080/test5.jpg, content=content5}, {imageUrl=http://localhost:8080/test6.jpg, content=content6}]", "상어", LocalDateTime.now(), 0, 0, 0);
-            PostDAO postDAO4 = new PostDAO(3, "코딩좀비", "테스트 제목4", "[{imageUrl=http://localhost:8080/test7.jpg, content=content7}, {imageUrl=http://localhost:8080/test8.jpg, content=content8}]", "도마뱀", LocalDateTime.now(), 4, 0, 0);
-            PostDAO postDAO5 = new PostDAO(4, "괴물", "테스트 제목5", "[{imageUrl=http://localhost:8080/test9.jpg, content=content9}, {imageUrl=http://localhost:8080/test10.jpg, content=content10}]", "뱀", LocalDateTime.now(), 6, 0, 0);
-            PostDAO postDAO6 = new PostDAO(5, "카페인중독", "테스트 제목6", "[{imageUrl=http://localhost:8080/test11.jpg, content=content11}, {imageUrl=http://localhost:8080/test12.jpg, content=content12}]", "상어", LocalDateTime.now(), 2, 0, 0);
-            PostDAO postDAO7 = new PostDAO(6, "괴물", "테스트 제목7", "[{imageUrl=http://localhost:8080/test13.jpg, content=content13}, {imageUrl=http://localhost:8080/test14.jpg, content=content14}]", "도마뱀", LocalDateTime.now(), 3, 0, 0);
-            PostDAO postDAO8 = new PostDAO(7, "코딩좀비", "테스트 제목8", "[{imageUrl=http://localhost:8080/test15.jpg, content=content15}, {imageUrl=http://localhost:8080/test16.jpg, content=content16}]", "뱀", LocalDateTime.now(), 6, 0, 0);
-            PostDAO postDAO9 = new PostDAO(8, "카페인중독", "테스트 제목9", "[{imageUrl=http://localhost:8080/test17.jpg, content=content17}, {imageUrl=http://localhost:8080/test18.jpg, content=content18}]", "상어", LocalDateTime.now(), 9, 0, 0);
-            PostDAO postDAO10 = new PostDAO(9, "괴물", "테스트 제목10", "[{imageUrl=http://localhost:8080/test19.jpg, content=content19}, {imageUrl=http://localhost:8080/test20.jpg, content=content20}]", "도마뱀", LocalDateTime.now(), 7, 0, 0);
+            PostDAO postDAO1 = new PostDAO(0, "코딩좀비", "테스트 제목1", "[{imageUrl=http://localhost:8080/test1.jpg, content=content1}, {imageUrl=http://localhost:8080/test2.jpg, content=content2}]", Category.FROG, LocalDateTime.now(), 3, 1, 0);
+            PostDAO postDAO2 = new PostDAO(1, "괴물", "테스트 제목2", "[{imageUrl=http://localhost:8080/test3.jpg, content=content3}, {imageUrl=http://localhost:8080/test4.jpg, content=content4}]", Category.REPTILE, LocalDateTime.now(), 2, 6, 0);
+            PostDAO postDAO3 = new PostDAO(2, "카페인중독", "테스트 제목3", "[{imageUrl=http://localhost:8080/test5.jpg, content=content5}, {imageUrl=http://localhost:8080/test6.jpg, content=content6}]", Category.SNAKE, LocalDateTime.now(), 0, 0, 0);
+            PostDAO postDAO4 = new PostDAO(3, "코딩좀비", "테스트 제목4", "[{imageUrl=http://localhost:8080/test7.jpg, content=content7}, {imageUrl=http://localhost:8080/test8.jpg, content=content8}]", Category.TURTLE, LocalDateTime.now(), 4, 0, 0);
+            PostDAO postDAO5 = new PostDAO(4, "괴물", "테스트 제목5", "[{imageUrl=http://localhost:8080/test9.jpg, content=content9}, {imageUrl=http://localhost:8080/test10.jpg, content=content10}]", Category.FROG, LocalDateTime.now(), 6, 0, 0);
+            PostDAO postDAO6 = new PostDAO(5, "카페인중독", "테스트 제목6", "[{imageUrl=http://localhost:8080/test11.jpg, content=content11}, {imageUrl=http://localhost:8080/test12.jpg, content=content12}]", Category.REPTILE, LocalDateTime.now(), 2, 0, 0);
+            PostDAO postDAO7 = new PostDAO(6, "괴물", "테스트 제목7", "[{imageUrl=http://localhost:8080/test13.jpg, content=content13}, {imageUrl=http://localhost:8080/test14.jpg, content=content14}]", Category.SNAKE, LocalDateTime.now(), 3, 0, 0);
+            PostDAO postDAO8 = new PostDAO(7, "코딩좀비", "테스트 제목8", "[{imageUrl=http://localhost:8080/test15.jpg, content=content15}, {imageUrl=http://localhost:8080/test16.jpg, content=content16}]", Category.TURTLE, LocalDateTime.now(), 6, 0, 0);
+            PostDAO postDAO9 = new PostDAO(8, "카페인중독", "테스트 제목9", "[{imageUrl=http://localhost:8080/test17.jpg, content=content17}, {imageUrl=http://localhost:8080/test18.jpg, content=content18}]", Category.FROG, LocalDateTime.now(), 9, 0, 0);
+            PostDAO postDAO10 = new PostDAO(9, "괴물", "테스트 제목10", "[{imageUrl=http://localhost:8080/test19.jpg, content=content19}, {imageUrl=http://localhost:8080/test20.jpg, content=content20}]", Category.REPTILE, LocalDateTime.now(), 7, 0, 0);
 
             postRepositoryJPA.save(postDAO1);
             postRepositoryJPA.save(postDAO2);
