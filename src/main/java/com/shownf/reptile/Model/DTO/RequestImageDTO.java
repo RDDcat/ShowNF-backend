b877f646c0a2e1020359476a084504462bce5109
Lee <lsmin476@naver.com>
2023-09-03 18:55:24 +0900

    |SM UPDATE| DTO, Repository 이미지 전체 조회 페이징을 위한 객체 및 레포지토리 저장

--- src/main/java/com/shownf/reptile/Model/DTO/RequestImageDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/RequestImageDTO.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.Model.DTO;public class RequestImageDTO {
+package com.shownf.reptile.Model.DTO;
+
+import lombok.Data;
+
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+public class RequestImageDTO {
+    @Id
+    Long imageId;
+    String imageName;
+    String imageUrl;
+    LocalDateTime uploadTime;
+    Integer heartCount;
 }
--- src/main/java/com/shownf/reptile/repository/ImageRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/ImageRepositoryJPA.java
@@ -1,7 +1,11 @@
 package com.shownf.reptile.repository;
 
 import com.shownf.reptile.Model.entity.ImageDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ImageRepositoryJPA extends JpaRepository<ImageDAO, Long> {
+
+    Page<ImageDAO> findAll(Pageable pageable);
 }
