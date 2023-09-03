5cc7a96124542447ea1c9ae394bb36efdcdc882b
Lee <lsmin476@naver.com>
2023-09-03 18:55:46 +0900

    |SM UPDATE| small Bean Image Page로 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetImagesDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetImagesDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetImagesDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.ImageDAO;
+import com.shownf.reptile.repository.ImageRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetImagesDAOBean {
+
+    ImageRepositoryJPA imageRepositoryJPA;
+
+    @Autowired
+    public GetImagesDAOBean(ImageRepositoryJPA imageRepositoryJPA) {
+        this.imageRepositoryJPA = imageRepositoryJPA;
+    }
+
+    // 이미지 전체 찾기
+    public Page<ImageDAO> exec(Pageable pageable){
+        return imageRepositoryJPA.findAll(pageable);
+    }
 }
