bbf40103bd4fe941640628d45385050c6648e3db
Lee <lsmin476@naver.com>
2023-09-03 18:06:34 +0900

    |SM UPDATE| small Bean 카테고리별 Post Page로 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetCategoryPostsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetCategoryPostsDAOBean.java
@@ -1,2 +1,25 @@
-package com.shownf.reptile.bean.small;public class GetCategoryPostsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.Enum.Category;
+import com.shownf.reptile.Model.entity.PostDAO;
+import com.shownf.reptile.repository.PostRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetCategoryPostsDAOBean {
+
+    PostRepositoryJPA postRepositoryJPA;
+
+    @Autowired
+    public GetCategoryPostsDAOBean(PostRepositoryJPA postRepositoryJPA) {
+        this.postRepositoryJPA = postRepositoryJPA;
+    }
+
+    // 카테고리별 게시물 찾기
+    public Page<PostDAO> exec(String category, Pageable pageable){
+        return postRepositoryJPA.findByCategory(Category.valueOf(category), pageable);
+    }
 }
--- src/main/java/com/shownf/reptile/repository/PostRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/PostRepositoryJPA.java
@@ -1,5 +1,6 @@
 package com.shownf.reptile.repository;
 
+import com.shownf.reptile.Model.Enum.Category;
 import com.shownf.reptile.Model.entity.PostDAO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepositoryJPA extends JpaRepository<PostDAO, Long> {
     Page<PostDAO> findAll(Pageable pageable);
+
+    Page<PostDAO> findByCategory(Category category, Pageable pageable);
 }
