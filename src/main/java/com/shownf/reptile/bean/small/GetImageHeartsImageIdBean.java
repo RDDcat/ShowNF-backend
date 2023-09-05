e92062de17a03f2b0843d981919b8a5ffc127fef
Lee <lsmin476@naver.com>
2023-09-05 21:22:14 +0900

    |SM UPDATE| small Bean 이미지 좋아요 객체에서 이미지 아이디 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetImageHeartsImageIdBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetImageHeartsImageIdBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetImageHeartsImageIdBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.ImageHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetImageHeartsImageIdBean {
+
+    // 이미지 좋아요에서 이미지 아이디 가져오기
+    public List<Long> exec(List<ImageHeartDAO> imageHeartDAOs){
+
+        List<Long> userIds = new ArrayList<>();
+
+        for (ImageHeartDAO imageHeartDAO : imageHeartDAOs)
+            userIds.add(imageHeartDAO.getImageId());
+
+        return userIds;
+    }
 }
