7864174657d0afad1a999b44b2be37d622600c34
Lee <lsmin476@naver.com>
2023-09-05 20:45:35 +0900

    |SM UPDATE| small Bean 게시물 아이디로 게시물 DAOs 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetPostsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostsDAOBean.java
@@ -7,6 +7,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class GetPostsDAOBean {
 
@@ -20,5 +23,16 @@ public class GetPostsDAOBean {
     // 게시물 찾기
     public Page<PostDAO> exec(Pageable pageable){
         return postRepositoryJPA.findAll(pageable);
+    }
+
+    // 게시물 아이디로 게시물 찾기
+    public List<PostDAO> exec(List<Long> postIds){
+
+        List<PostDAO> postDAOs = new ArrayList<>();
+
+        for (Long postId : postIds)
+            postDAOs.add(postRepositoryJPA.findById(postId).get());
+
+        return postDAOs;
     }
 }
