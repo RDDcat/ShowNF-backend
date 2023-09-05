c85ad381fdd68eda400628e45cf476133a44d26f
Lee <lsmin476@naver.com>
2023-09-05 20:44:34 +0900

    |SM UPDATE| small Bean 게시물 좋아요 객체에서 게시물 아이디 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetPostHeartsPostIdBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostHeartsPostIdBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetPostIdPostHeartsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.PostHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetPostHeartsPostIdBean {
+
+    // 좋아요 게시물에서 게시물 아이디 가져오기
+    public List<Long> exec(List<PostHeartDAO> postHeartDAOs){
+
+        List<Long> postIds = new ArrayList<>();
+
+        for (PostHeartDAO postHeartDAO : postHeartDAOs)
+            postIds.add(postHeartDAO.getPostId());
+
+        return postIds;
+    }
 }
