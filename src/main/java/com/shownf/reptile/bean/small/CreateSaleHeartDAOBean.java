ebd03f844e57b4648179e744c52d87ff906b3c89
Lee <lsmin476@naver.com>
2023-09-05 21:53:24 +0900

    |SM UPDATE| small Bean 분양글 좋아요 DAO 객체 생성하기

--- src/main/java/com/shownf/reptile/bean/small/CreateSaleHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateSaleHeartDAOBean.java
@@ -1,2 +1,27 @@
-package com.shownf.reptile.bean.small;public class CreateSaleHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.RequestSaleHeartSaveDTO;
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class CreateSaleHeartDAOBean {
+
+    // 분양글 좋아요 저장시 DAO 생성
+    public SaleHeartDAO exec(Long saleHeartId, RequestSaleHeartSaveDTO requestSaleHeartSaveDTO){
+
+        // 분양글 아이디
+        Long saleId = requestSaleHeartSaveDTO.getSaleId();
+
+        // 유저 아이디
+        String userId = requestSaleHeartSaveDTO.getUserId();
+
+        // 업로드 시간
+        LocalDateTime uploadTime = LocalDateTime.now();
+
+        // DAO 반환
+        return new SaleHeartDAO(saleHeartId, saleId, userId, uploadTime);
+    }
 }
