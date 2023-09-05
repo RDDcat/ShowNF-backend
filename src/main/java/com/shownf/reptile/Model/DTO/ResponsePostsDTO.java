3c3ddac2bdb8fc24685d4bbfbebad9fbb6ad15e2
Lee <lsmin476@naver.com>
2023-09-05 20:43:09 +0900

    |SM UPDATE| DTO, Repository 유저가 좋아요 누른 게시물 가져오기 위한 객체 및 레포지토리 저장

--- src/main/java/com/shownf/reptile/Model/DTO/ResponsePostsDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/ResponsePostsDTO.java
@@ -1,2 +1,18 @@
-package com.shownf.reptile.Model.DTO;public class ResponsePostsDTO {
+package com.shownf.reptile.Model.DTO;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ResponsePostsDTO {
+    long postId;
+    String userId;
+    String title;
+    String content;
+    String category;
+    LocalDateTime uploadTime;
+    Integer heartCount;
+    Integer commentCount;
+    Integer viewCount;
 }
--- src/main/java/com/shownf/reptile/repository/PostHeartRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/PostHeartRepositoryJPA.java
@@ -3,5 +3,8 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.Model.entity.PostHeartDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostHeartRepositoryJPA extends JpaRepository<PostHeartDAO, Long> {
+    List<PostHeartDAO> findByUserId(String userId);
 }
