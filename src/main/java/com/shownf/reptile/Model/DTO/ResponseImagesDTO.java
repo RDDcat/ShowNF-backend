071baaa0301d416825cff005b79d41c2e1bd3a59
Lee <lsmin476@naver.com>
2023-09-05 21:21:37 +0900

    |SM UPDATE| DTO, Repository 유저가 좋아요 누른 이미지 가져오기 위한 객체 및 레포지토리 저장

--- src/main/java/com/shownf/reptile/Model/DTO/ResponseImagesDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/ResponseImagesDTO.java
@@ -1,2 +1,14 @@
-package com.shownf.reptile.Model.DTO;public class ResponseImagesDTO {
+package com.shownf.reptile.Model.DTO;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class ResponseImagesDTO {
+    Long imageId;
+    String imageName;
+    String imageUrl;
+    LocalDateTime uploadTime;
+    Integer heartCount;
 }
--- src/main/java/com/shownf/reptile/repository/ImageHeartRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/ImageHeartRepositoryJPA.java
@@ -3,5 +3,8 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.Model.entity.ImageHeartDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageHeartRepositoryJPA extends JpaRepository<ImageHeartDAO, Long> {
+    List<ImageHeartDAO> findByUserId(String userId);
 }
