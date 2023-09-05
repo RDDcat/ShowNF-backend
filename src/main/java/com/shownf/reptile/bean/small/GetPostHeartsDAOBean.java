f28f99fa53f221b2b0aa56f01432d720d2ba17a3
Lee <lsmin476@naver.com>
2023-09-05 20:44:03 +0900

    |SM UPDATE| small Bean 유저 아이디로 게시물 좋아요 객체 찾기

--- src/main/java/com/shownf/reptile/bean/small/GetPostHeartsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPostHeartsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetPostHeartsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.PostHeartDAO;
+import com.shownf.reptile.repository.PostHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetPostHeartsDAOBean {
+
+    PostHeartRepositoryJPA postHeartRepositoryJPA;
+
+    @Autowired
+    public GetPostHeartsDAOBean(PostHeartRepositoryJPA postHeartRepositoryJPA) {
+        this.postHeartRepositoryJPA = postHeartRepositoryJPA;
+    }
+
+    // 좋아요 아이디로 좋아요 객체 찾기
+    public List<PostHeartDAO> exec(String userId){
+        return postHeartRepositoryJPA.findByUserId(userId);
+    }
 }
