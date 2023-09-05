2e9a9ed817dc9ac9f012769fdcf22c8f7a53652e
Lee <lsmin476@naver.com>
2023-09-05 21:21:57 +0900

    |SM UPDATE| small Bean 유저 아이디로 이미지 좋아요 객체 찾기

--- src/main/java/com/shownf/reptile/bean/small/GetImageHeartsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetImageHeartsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetImageHeartsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.ImageHeartDAO;
+import com.shownf.reptile.repository.ImageHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetImageHeartsDAOBean {
+
+    ImageHeartRepositoryJPA imageHeartRepositoryJPA;
+
+    @Autowired
+    public GetImageHeartsDAOBean(ImageHeartRepositoryJPA imageHeartRepositoryJPA) {
+        this.imageHeartRepositoryJPA = imageHeartRepositoryJPA;
+    }
+
+    // 유저 아이디로 좋아요 객체 찾기
+    public List<ImageHeartDAO> exec(String userId){
+        return imageHeartRepositoryJPA.findByUserId(userId);
+    }
 }
