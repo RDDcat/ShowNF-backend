00e8adf1987ee1f6cb2eeb38b8deb9f970266dcc
Lee <lsmin476@naver.com>
2023-09-05 21:53:42 +0900

    |SM UPDATE| small Bean 분양글 좋아요 repository 저장하기

--- src/main/java/com/shownf/reptile/bean/small/SaveSaleHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveSaleHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class SaveSaleHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import com.shownf.reptile.repository.SaleHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveSaleHeartDAOBean {
+
+    SaleHeartRepositoryJPA saleHeartRepositoryJPA;
+
+    @Autowired
+    public SaveSaleHeartDAOBean(SaleHeartRepositoryJPA saleHeartRepositoryJPA) {
+        this.saleHeartRepositoryJPA = saleHeartRepositoryJPA;
+    }
+
+    // 분양글 좋아요 저장
+    public void exec(SaleHeartDAO saleHeartDAO){
+        saleHeartRepositoryJPA.save(saleHeartDAO);
+    }
 }
