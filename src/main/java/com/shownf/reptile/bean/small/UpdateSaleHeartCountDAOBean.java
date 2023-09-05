6e71f4e3b44ff844f256ebbae8849a2e501dbf92
Lee <lsmin476@naver.com>
2023-09-05 21:53:56 +0900

    |SM UPDATE| small Bean 분양글 좋아요 갯수 추가에 따른 수정

--- src/main/java/com/shownf/reptile/bean/small/UpdateSaleHeartCountDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/UpdateSaleHeartCountDAOBean.java
@@ -1,2 +1,34 @@
-package com.shownf.reptile.bean.small;public class UpdateSaleHeartCountDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleDAO;
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import com.shownf.reptile.repository.SaleRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateSaleHeartCountDAOBean {
+
+    SaleRepositoryJPA saleRepositoryJPA;
+
+    @Autowired
+    public UpdateSaleHeartCountDAOBean(SaleRepositoryJPA saleRepositoryJPA) {
+        this.saleRepositoryJPA = saleRepositoryJPA;
+    }
+
+    // 분양글 좋아요 갯수 추가
+    public SaleDAO exec(SaleHeartDAO saleHeartDAO){
+
+        // saleId 가져오기
+        Long saleId = saleHeartDAO.getSaleId();
+
+        // saleId 로 분양글 찾기
+        SaleDAO saleDAO = saleRepositoryJPA.findById(saleId).get();
+
+        // 분양글 좋아요 수 1 증가
+        saleDAO.setHeartCount(saleDAO.getHeartCount() + 1);
+
+        // 분양글 반환
+        return saleDAO;
+    }
 }
