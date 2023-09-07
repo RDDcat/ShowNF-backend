5477a9ab2053d2c8fbe00d2279c60d5161066a72
Lee <lsmin476@naver.com>
2023-09-07 17:17:55 +0900

    |SM UPDATE| small Bean 유저가 좋아요한 분양글 DTOs 생성

--- src/main/java/com/shownf/reptile/bean/small/CreateSalesDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateSalesDTOBean.java
@@ -1,2 +1,41 @@
-package com.shownf.reptile.bean.small;public class CreateSalesDTOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.ResponseSalesDTO;
+import com.shownf.reptile.Model.entity.SaleDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CreateSalesDTOBean {
+
+    // 분양글 아이디로 분양글 조회시 DTO 생성
+    public List<ResponseSalesDTO> exec(List<SaleDAO> saleDAOs){
+
+        List<ResponseSalesDTO> responseSalesDTOs = new ArrayList<>();
+
+        // DTO 객체에 게시물 정보 넘기기
+        for (SaleDAO saleDAO: saleDAOs) {
+            ResponseSalesDTO responseSalesDTO = new ResponseSalesDTO();
+
+            responseSalesDTO.setSaleId(saleDAO.getSaleId());
+            responseSalesDTO.setUserId(saleDAO.getUserId());
+            responseSalesDTO.setImageUrl(saleDAO.getImageUrl());
+            responseSalesDTO.setIndividual(saleDAO.getIndividual());
+            responseSalesDTO.setFirstSpecies(saleDAO.getFirstSpecies());
+            responseSalesDTO.setSecondSpecies(saleDAO.getSecondSpecies());
+            responseSalesDTO.setHeartCount(saleDAO.getHeartCount());
+            responseSalesDTO.setBirthday(saleDAO.getBirthday());
+            responseSalesDTO.setWeight(saleDAO.getWeight());
+            responseSalesDTO.setSpeciesExplain(saleDAO.getSpeciesExplain());
+            responseSalesDTO.setPrice(saleDAO.getPrice());
+            responseSalesDTO.setHeartCount(saleDAO.getHeartCount());
+
+            responseSalesDTOs.add(responseSalesDTO);
+        }
+
+        // DTO 반환
+        return responseSalesDTOs;
+    }
 }
