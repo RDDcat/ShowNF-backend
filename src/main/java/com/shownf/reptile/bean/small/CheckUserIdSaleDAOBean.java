3315e7f222deb5d9eb81a0facc839a106ad3c5f9
Lee <lsmin476@naver.com>
2023-09-07 15:39:12 +0900

    |SM UPDATE| small Bean 분양글 userId 확인하기

--- src/main/java/com/shownf/reptile/bean/small/CheckUserIdSaleDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckUserIdSaleDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckUserIdSaleDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.RequestSaleHeartDeleteDTO;
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckUserIdSaleDAOBean {
+
+    // 분양글 좋아요 userId 판별
+    public boolean exec(SaleHeartDAO saleHeartDAO, RequestSaleHeartDeleteDTO requestSaleHeartDeleteDTO){
+        if (saleHeartDAO.getUserId().equals(requestSaleHeartDeleteDTO.getUserId()))
+            return true;
+        return false;
+    }
 }
