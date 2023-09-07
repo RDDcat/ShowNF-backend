f5d7f1b40eca05fa5689b7c362924b69910f39fa
Lee <lsmin476@naver.com>
2023-09-07 15:38:40 +0900

    |SM UPDATE| small Bean 분양글 좋아요 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetSaleHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetSaleHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetSaleHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import com.shownf.reptile.repository.SaleHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetSaleHeartDAOBean {
+
+    SaleHeartRepositoryJPA saleHeartRepositoryJPA;
+
+    @Autowired
+    public GetSaleHeartDAOBean(SaleHeartRepositoryJPA saleHeartRepositoryJPA) {
+        this.saleHeartRepositoryJPA = saleHeartRepositoryJPA;
+    }
+
+    // 분양글 좋아요 아이디로 객체 찾기
+    public SaleHeartDAO exec(Long saleHeartId){
+        return saleHeartRepositoryJPA.findById(saleHeartId).get();
+    }
 }
