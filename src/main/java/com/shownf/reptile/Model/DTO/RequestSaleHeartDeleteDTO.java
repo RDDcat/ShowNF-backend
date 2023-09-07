5358d2af474c2ab05c37ec74dabffd98b933bfbd
Lee <lsmin476@naver.com>
2023-09-07 15:37:52 +0900

    |SM UPDATE| DTO 분양글 좋아요 삭체 요청

--- src/main/java/com/shownf/reptile/Model/DTO/RequestSaleHeartDeleteDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/RequestSaleHeartDeleteDTO.java
@@ -1,2 +1,10 @@
-package com.shownf.reptile.Model.DTO;public class RequestSaleHeartDeleteDTO {
+package com.shownf.reptile.Model.DTO;
+
+import lombok.Data;
+
+@Data
+public class RequestSaleHeartDeleteDTO {
+    Long saleHeartId;
+    Long saleId;
+    String userId;
 }
