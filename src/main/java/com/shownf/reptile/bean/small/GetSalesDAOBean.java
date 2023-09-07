08a21b2f85fa2e74ff96ca7abdf865874b2fde98
Lee <lsmin476@naver.com>
2023-09-07 17:17:42 +0900

    |SM UPDATE| small Bean 분양글 아이디로 분양글 DAOs 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetSalesDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetSalesDAOBean.java
@@ -1,2 +1,31 @@
-package com.shownf.reptile.bean.small;public class GetSalesDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleDAO;
+import com.shownf.reptile.repository.SaleRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetSalesDAOBean {
+
+    SaleRepositoryJPA saleRepositoryJPA;
+
+    @Autowired
+    public GetSalesDAOBean(SaleRepositoryJPA saleRepositoryJPA) {
+        this.saleRepositoryJPA = saleRepositoryJPA;
+    }
+
+    // 분양글 아이디로 분양글 찾기
+    public List<SaleDAO> exec(List<Long> saleIds){
+
+        List<SaleDAO> saleDAOs = new ArrayList<>();
+
+        for (Long saleId : saleIds)
+            saleDAOs.add(saleRepositoryJPA.findById(saleId).get());
+
+        return saleDAOs;
+    }
 }
