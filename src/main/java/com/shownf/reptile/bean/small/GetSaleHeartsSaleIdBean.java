ba1d44e5ea262eb87235e53546f9aa5dd8d5ea77
Lee <lsmin476@naver.com>
2023-09-07 17:17:13 +0900

    |SM UPDATE| small Bean 분양글 좋아요 객체에서 분양글 아이디 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetSaleHeartsSaleIdBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetSaleHeartsSaleIdBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetSaleHeartsSaleIdBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class GetSaleHeartsSaleIdBean {
+
+    // 좋아요에서 분양글 아이디 가져오기
+    public List<Long> exec(List<SaleHeartDAO> saleHeartDAOs){
+
+        List<Long> saleIds = new ArrayList<>();
+
+        for (SaleHeartDAO saleHeartDAO : saleHeartDAOs)
+            saleIds.add(saleHeartDAO.getSaleId());
+
+        return saleIds;
+    }
 }
