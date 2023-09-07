f87eb6cfc624dfa3ade3b865bca7c933a67f56b8
Lee <lsmin476@naver.com>
2023-09-07 17:16:38 +0900

    |SM UPDATE| DTO, Repository 유저가 좋아요 누른 분양글 가져오기 위한 객체 및 레포지토리 저장

--- src/main/java/com/shownf/reptile/Model/DTO/ResponseSalesDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/ResponseSalesDTO.java
@@ -1,2 +1,19 @@
-package com.shownf.reptile.Model.DTO;public class ResponseSalesDTO {
+package com.shownf.reptile.Model.DTO;
+
+import lombok.Data;
+
+
+@Data
+public class ResponseSalesDTO {
+    Long saleId;
+    String userId;
+    String imageUrl;
+    String individual;
+    String firstSpecies;
+    String secondSpecies;
+    String birthday;
+    Double weight;
+    String speciesExplain;
+    Integer price;
+    Integer heartCount;
 }
--- src/main/java/com/shownf/reptile/repository/SaleHeartRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/SaleHeartRepositoryJPA.java
@@ -3,5 +3,8 @@ package com.shownf.reptile.repository;
 import com.shownf.reptile.Model.entity.SaleHeartDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleHeartRepositoryJPA extends JpaRepository<SaleHeartDAO, Long> {
+    List<SaleHeartDAO> findByUserId(String userId);
 }
