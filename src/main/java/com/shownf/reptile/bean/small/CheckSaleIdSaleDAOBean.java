a45e6589d7d9c4e45544ada14c61d491dac13010
Lee <lsmin476@naver.com>
2023-09-07 15:39:00 +0900

    |SM UPDATE| small Bean 분양글 saleId 확인하기

--- src/main/java/com/shownf/reptile/bean/small/CheckSaleIdSaleDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CheckSaleIdSaleDAOBean.java
@@ -1,2 +1,16 @@
-package com.shownf.reptile.bean.small;public class CheckSaleIdSaleDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.RequestSaleHeartDeleteDTO;
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckSaleIdSaleDAOBean {
+
+    // 좋아요 saleId 판별
+    public boolean exec(SaleHeartDAO saleHeartDAO, RequestSaleHeartDeleteDTO requestSaleHeartDeleteDTO){
+        if (saleHeartDAO.getSaleId().equals(requestSaleHeartDeleteDTO.getSaleId()))
+            return true;
+        return false;
+    }
 }
