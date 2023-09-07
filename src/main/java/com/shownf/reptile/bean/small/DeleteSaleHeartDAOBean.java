762399cf130302b277ff0f495e18583beb34675a
Lee <lsmin476@naver.com>
2023-09-07 15:40:02 +0900

    |SM UPDATE| small Bean 분양글 좋아요 repository 삭제하기

--- src/main/java/com/shownf/reptile/bean/small/DeleteSaleHeartDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/DeleteSaleHeartDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class DeleteSaleHeartDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import com.shownf.reptile.repository.SaleHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeleteSaleHeartDAOBean {
+
+    SaleHeartRepositoryJPA saleHeartRepositoryJPA;
+
+    @Autowired
+    public DeleteSaleHeartDAOBean(SaleHeartRepositoryJPA saleHeartRepositoryJPA) {
+        this.saleHeartRepositoryJPA = saleHeartRepositoryJPA;
+    }
+
+    // 분양글 좋아요 삭제
+    public void exec(SaleHeartDAO saleHeartDAO){
+        saleHeartRepositoryJPA.delete(saleHeartDAO);
+    }
 }
