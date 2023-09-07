9231f158ae70053d1a9b89409b71bd47ff67c4e1
Lee <lsmin476@naver.com>
2023-09-07 17:16:57 +0900

    |SM UPDATE| small Bean 유저 아이디로 분양글 좋아요 객체 찾기

--- src/main/java/com/shownf/reptile/bean/small/GetSaleHeartsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetSaleHeartsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetSaleHeartsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleHeartDAO;
+import com.shownf.reptile.repository.SaleHeartRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetSaleHeartsDAOBean {
+
+    SaleHeartRepositoryJPA saleHeartRepositoryJPA;
+
+    @Autowired
+    public GetSaleHeartsDAOBean(SaleHeartRepositoryJPA saleHeartRepositoryJPA) {
+        this.saleHeartRepositoryJPA = saleHeartRepositoryJPA;
+    }
+
+    // 좋아요 아이디로 좋아요 객체 찾기
+    public List<SaleHeartDAO> exec(String userId){
+        return saleHeartRepositoryJPA.findByUserId(userId);
+    }
 }
