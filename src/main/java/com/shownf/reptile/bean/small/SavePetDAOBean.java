45140a0ba71bcdead42a4ee4e05e930855b305b4
Lee <lsmin476@naver.com>
2023-09-10 19:23:06 +0900

    |SM UPDATE| small Bean Pet DAO 객체 생성 및 레포지토리 저장

--- src/main/java/com/shownf/reptile/bean/small/SavePetDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SavePetDAOBean.java
@@ -1,2 +1,51 @@
-package com.shownf.reptile.bean.small;public class SavePetDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.RequestPetSaveDTO;
+import com.shownf.reptile.Model.entity.PetDAO;
+import com.shownf.reptile.repository.PetRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SavePetDAOBean {
+
+    PetRepositoryJPA petRepositoryJPA;
+
+    @Autowired
+    public SavePetDAOBean(PetRepositoryJPA petRepositoryJPA) {
+        this.petRepositoryJPA = petRepositoryJPA;
+    }
+
+    // 마이펫 Repository 저장하기
+    public void exec(PetDAO petDAO){
+        petRepositoryJPA.save(petDAO);
+    }
+
+    // 마이펫 저장시 DAO 생성
+    public void exec(Long petId, RequestPetSaveDTO requestPetSaveDTO){
+
+        // 유저 아이디
+        String userId = requestPetSaveDTO.getUserId();
+
+        // 이미지 Url
+        String imageUrl = requestPetSaveDTO.getImageUrl().toString();
+
+        // 이름
+        String name = requestPetSaveDTO.getName();
+
+        // 첫번째 종
+        String firstSpecies = requestPetSaveDTO.getFirstSpecies();
+
+        // 두번째 종
+        String secondSpecies = requestPetSaveDTO.getSecondSpecies();
+
+        // 생일
+        String birthday = requestPetSaveDTO.getBirthday();
+
+        // 몸무게
+        Double weight = requestPetSaveDTO.getWeight();
+
+        exec(new PetDAO(petId, userId, imageUrl, name, firstSpecies, secondSpecies, birthday, weight));
+
+    }
 }
