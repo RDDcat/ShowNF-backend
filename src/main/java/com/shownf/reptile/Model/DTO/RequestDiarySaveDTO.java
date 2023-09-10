280c3b7301ce4f9eaf5badd6552e3d63a41db7ef
Lee <lsmin476@naver.com>
2023-09-11 00:51:21 +0900

    |SM UPDATE| DTO LocalTime 수정

--- src/main/java/com/shownf/reptile/Model/DTO/RequestDiarySaveDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/RequestDiarySaveDTO.java
@@ -2,8 +2,6 @@ package com.shownf.reptile.Model.DTO;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
-
 @Data
 public class RequestDiarySaveDTO {
     Long petId;
@@ -13,5 +11,4 @@ public class RequestDiarySaveDTO {
     String size;
     Double weight;
     String memo;
-    LocalDateTime uploadTime;
 }
