e5f1640387ec6d66abb26541e775b08fdb1c5d25
Lee <lsmin476@naver.com>
2023-09-10 20:12:00 +0900

    |SM UPDATE| small Bean 마이펫 DAO DTO로 변환

--- src/main/java/com/shownf/reptile/bean/small/CreatePetDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreatePetDTOBean.java
@@ -1,2 +1,27 @@
-package com.shownf.reptile.bean.small;public class CreatPetDTOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.RequestPetDTO;
+import com.shownf.reptile.Model.entity.PetDAO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CreatePetDTOBean {
+
+    // 마이펫 조회 시 DTO 생성
+    public RequestPetDTO exec(PetDAO petDAO){
+        RequestPetDTO requestPetDTO = new RequestPetDTO();
+
+        // DTO 객체에 마이펫 정보 넘기기
+        requestPetDTO.setPetId(petDAO.getPetId());
+        requestPetDTO.setUserId(petDAO.getUserId());
+        requestPetDTO.setImageUrl(petDAO.getImageUrl());
+        requestPetDTO.setName(petDAO.getName());
+        requestPetDTO.setFirstSpecies(petDAO.getFirstSpecies());
+        requestPetDTO.setSecondSpecies(petDAO.getSecondSpecies());
+        requestPetDTO.setBirthday(petDAO.getBirthday());
+        requestPetDTO.setWeight(petDAO.getWeight());
+
+        // DTO 반환
+        return requestPetDTO;
+    }
 }
