c6a64a9af187e87618345b28f91e867ca7ffa743
Lee <lsmin476@naver.com>
2023-09-13 00:39:07 +0900

    |SM UPDATE| small Bean 다이어리 월별 DAOs DTOs 변환

--- src/main/java/com/shownf/reptile/bean/small/CreateDiarysDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateDiarysDTOBean.java
@@ -1,2 +1,32 @@
-package com.shownf.reptile.bean.small;public class CreateDiarysDTOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.DTO.ResponseDiarysDTO;
+import com.shownf.reptile.Model.entity.DiaryDAO;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CreateDiarysDTOBean {
+
+    // 월별로 다이어리 조회시 DTO 생성
+    public List<ResponseDiarysDTO> exec(List<DiaryDAO> diaryDAOs) {
+
+        List<ResponseDiarysDTO> responseDiarysDTOs = new ArrayList<>();
+
+        // DTO 객체에 게시물 정보 넘기기
+        for (DiaryDAO diaryDAO : diaryDAOs) {
+            ResponseDiarysDTO responseDiarysDTO = new ResponseDiarysDTO();
+
+            responseDiarysDTO.setDiaryId(diaryDAO.getDiaryId());
+            responseDiarysDTO.setPetId(diaryDAO.getPetId());
+            responseDiarysDTO.setUserId(diaryDAO.getUserId());
+            responseDiarysDTO.setDate(diaryDAO.getDate());
+
+            responseDiarysDTOs.add(responseDiarysDTO);
+        }
+
+        return responseDiarysDTOs;
+    }
 }
