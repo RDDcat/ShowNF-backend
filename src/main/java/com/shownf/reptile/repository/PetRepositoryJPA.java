b18cc98e09b5e78f4654e2cf147d8d73c32a843b
Lee <lsmin476@naver.com>
2023-09-12 14:17:16 +0900

    |SM UPDATE| Repository 마이펫 유저별 조회 페이징을 위한 레포지토리 저장

--- src/main/java/com/shownf/reptile/repository/PetRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/PetRepositoryJPA.java
@@ -1,7 +1,11 @@
 package com.shownf.reptile.repository;
 
 import com.shownf.reptile.Model.entity.PetDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PetRepositoryJPA extends JpaRepository<PetDAO, Long> {
+
+    Page<PetDAO> findByUserId(String userId, Pageable pageable);
 }
