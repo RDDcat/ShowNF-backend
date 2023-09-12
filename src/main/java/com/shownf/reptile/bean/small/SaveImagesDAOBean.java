54ff9016259b4e3cf7fe45b9df4df2c30afd9a72
Lee <lsmin476@naver.com>
2023-09-12 15:04:32 +0900

    |SM UPDATE| small Bean 마이펫 저장시 이미지 저장 로직 추가

--- src/main/java/com/shownf/reptile/bean/small/SaveImagesDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/SaveImagesDAOBean.java
@@ -1,5 +1,6 @@
 package com.shownf.reptile.bean.small;
 
+import com.shownf.reptile.Model.DTO.RequestPetSaveDTO;
 import com.shownf.reptile.Model.DTO.RequestPostSaveDTO;
 import com.shownf.reptile.Model.entity.ImageDAO;
 import com.shownf.reptile.repository.ImageRepositoryJPA;
@@ -37,6 +38,39 @@ public class SaveImagesDAOBean {
 
         // 입력받은 postContent 갯수
         List<Map<String, String>> list = requestPostSaveDTO.getContent();
+        int count = list.size();
+
+        for (int i = 0; i < count; i++) {
+
+            // imageId 생성
+            Long imageId = createUniqueIdBean.exec();
+
+            // 이미지 이름
+            String imageName = list.get(i).get("imageUrl").substring(list.get(i).get("imageUrl").lastIndexOf("8080/") + 5);
+
+            // 이미지 Url
+            String imageUrl = list.get(i).get("imageUrl");
+
+            // 업로드 시간
+            LocalDateTime uploadTime = LocalDateTime.now();
+
+            // 이미지 좋아요 갯수
+            Integer heartCount = 0;
+
+            // 이미지 DAO 저장
+            imageDAOs.add(new ImageDAO(imageId, imageName, imageUrl, uploadTime, heartCount));
+
+        }
+        exec(imageDAOs);
+    }
+
+    // 마이펫 저장시 이미지 저장
+    public void exec(RequestPetSaveDTO requestPetSaveDTO) {
+
+        List<ImageDAO> imageDAOs = new ArrayList<>();
+
+        // 입력받은 postContent 갯수
+        List<Map<String, String>> list = requestPetSaveDTO.getImageUrl();
         int count = list.size();
 
         for (int i = 0; i < count; i++) {
