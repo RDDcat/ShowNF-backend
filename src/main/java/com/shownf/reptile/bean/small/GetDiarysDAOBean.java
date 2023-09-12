a42c1032c87ee4090454c0c1f5ea58e5ea5cfe70
Lee <lsmin476@naver.com>
2023-09-13 00:38:47 +0900

    |SM UPDATE| small Bean 다이어리 월별 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetDiarysDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetDiarysDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetDiarysDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.DiaryDAO;
+import com.shownf.reptile.repository.DiaryRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class GetDiarysDAOBean {
+
+    DiaryRepositoryJPA diaryRepositoryJPA;
+
+    @Autowired
+    public GetDiarysDAOBean(DiaryRepositoryJPA diaryRepositoryJPA) {
+        this.diaryRepositoryJPA = diaryRepositoryJPA;
+    }
+
+    // 월별로 다이어리 조회
+    public List<DiaryDAO> exec(String month){
+        return diaryRepositoryJPA.findByMonth(month);
+    }
 }
