a425770650fe1a24f08db72d0e943a0181eeba40
Lee <lsmin476@naver.com>
2023-09-12 14:27:50 +0900

    |SM UPDATE| small Bean 마이펫 Page로 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetPetsDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetPetsDAOBean.java
@@ -1,2 +1,24 @@
-package com.shownf.reptile.bean.small;public class GetPetsDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.PetDAO;
+import com.shownf.reptile.repository.PetRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetPetsDAOBean {
+
+    PetRepositoryJPA petRepositoryJPA;
+
+    @Autowired
+    public GetPetsDAOBean(PetRepositoryJPA petRepositoryJPA) {
+        this.petRepositoryJPA = petRepositoryJPA;
+    }
+
+    // 유저 아이디로 마이펫 전체 찾기
+    public Page<PetDAO> exec(String userId, Pageable pageable){
+        return petRepositoryJPA.findByUserId(userId, pageable);
+    }
 }
