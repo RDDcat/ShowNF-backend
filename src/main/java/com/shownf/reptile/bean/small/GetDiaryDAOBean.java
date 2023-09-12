e307ae54f648a95631d1d9b2a929c9db867aa70b
Lee <lsmin476@naver.com>
2023-09-12 21:41:49 +0900

    |SM UPDATE| small Bean 다이어리 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetDiaryDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetDiaryDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetDiaryDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.DiaryDAO;
+import com.shownf.reptile.repository.DiaryRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetDiaryDAOBean {
+
+    DiaryRepositoryJPA diaryRepositoryJPA;
+
+    @Autowired
+    public GetDiaryDAOBean(DiaryRepositoryJPA diaryRepositoryJPA) {
+        this.diaryRepositoryJPA = diaryRepositoryJPA;
+    }
+
+    // 다이어리 찾기
+    public DiaryDAO exec(Long diaryId){
+        return diaryRepositoryJPA.findById(diaryId).get();
+    }
 }
