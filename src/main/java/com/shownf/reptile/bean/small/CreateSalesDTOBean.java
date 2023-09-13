0368ba0496df7d7eee173d18653729a6f1b21660
Lee <lsmin476@naver.com>
2023-09-13 16:45:58 +0900

    |SM UPDATE| small Bean 분양글 DAOs DTOs 변환

--- src/main/java/com/shownf/reptile/bean/small/CreateSalesDTOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/CreateSalesDTOBean.java
@@ -1,7 +1,10 @@
 package com.shownf.reptile.bean.small;
 
-import com.shownf.reptile.Model.DTO.ResponseSalesDTO;
+import com.shownf.reptile.Model.DTO.ResponseSaleDTO;
 import com.shownf.reptile.Model.entity.SaleDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -11,33 +14,64 @@ import java.util.List;
 public class CreateSalesDTOBean {
 
     // 분양글 아이디로 분양글 조회시 DTO 생성
-    public List<ResponseSalesDTO> exec(List<SaleDAO> saleDAOs){
+    public List<ResponseSaleDTO> exec(List<SaleDAO> saleDAOs){
 
-        List<ResponseSalesDTO> responseSalesDTOs = new ArrayList<>();
+        List<ResponseSaleDTO> responseSalesDTOs = new ArrayList<>();
 
         // DTO 객체에 게시물 정보 넘기기
         for (SaleDAO saleDAO: saleDAOs) {
-            ResponseSalesDTO responseSalesDTO = new ResponseSalesDTO();
+            ResponseSaleDTO responseSaleDTO = new ResponseSaleDTO();
 
-            responseSalesDTO.setSaleId(saleDAO.getSaleId());
-            responseSalesDTO.setPetId(saleDAO.getPetId());
-            responseSalesDTO.setUserId(saleDAO.getUserId());
-            responseSalesDTO.setImageUrl(saleDAO.getImageUrl());
-            responseSalesDTO.setIndividual(saleDAO.getIndividual());
-            responseSalesDTO.setFirstSpecies(saleDAO.getFirstSpecies());
-            responseSalesDTO.setSecondSpecies(saleDAO.getSecondSpecies());
-            responseSalesDTO.setHeartCount(saleDAO.getHeartCount());
-            responseSalesDTO.setBirthday(saleDAO.getBirthday());
-            responseSalesDTO.setWeight(saleDAO.getWeight());
-            responseSalesDTO.setSpeciesExplain(saleDAO.getSpeciesExplain());
-            responseSalesDTO.setPrice(saleDAO.getPrice());
-            responseSalesDTO.setHeartCount(saleDAO.getHeartCount());
-            responseSalesDTO.setSalePlatform(saleDAO.getSalePlatform().name());
+            responseSaleDTO.setSaleId(saleDAO.getSaleId());
+            responseSaleDTO.setPetId(saleDAO.getPetId());
+            responseSaleDTO.setUserId(saleDAO.getUserId());
+            responseSaleDTO.setImageUrl(saleDAO.getImageUrl());
+            responseSaleDTO.setIndividual(saleDAO.getIndividual());
+            responseSaleDTO.setFirstSpecies(saleDAO.getFirstSpecies());
+            responseSaleDTO.setSecondSpecies(saleDAO.getSecondSpecies());
+            responseSaleDTO.setHeartCount(saleDAO.getHeartCount());
+            responseSaleDTO.setBirthday(saleDAO.getBirthday());
+            responseSaleDTO.setWeight(saleDAO.getWeight());
+            responseSaleDTO.setSpeciesExplain(saleDAO.getSpeciesExplain());
+            responseSaleDTO.setPrice(saleDAO.getPrice());
+            responseSaleDTO.setHeartCount(saleDAO.getHeartCount());
+            responseSaleDTO.setSalePlatform(saleDAO.getSalePlatform().name());
 
-            responseSalesDTOs.add(responseSalesDTO);
+            responseSalesDTOs.add(responseSaleDTO);
         }
 
         // DTO 반환
         return responseSalesDTOs;
+    }
+
+    // 분양글 Page 형태로 전체 조회시 DTO 생성
+    public Page<ResponseSaleDTO> exec(Pageable pageable, Page<SaleDAO> saleDAOs){
+
+        List<ResponseSaleDTO> responseSaleDTOs = new ArrayList<>();
+
+        // DTO 객체에 분양글 정보 넘기기
+        for (SaleDAO saleDAO: saleDAOs) {
+            ResponseSaleDTO responseSaleDTO = new ResponseSaleDTO();
+
+            responseSaleDTO.setSaleId(saleDAO.getSaleId());
+            responseSaleDTO.setPetId(saleDAO.getPetId());
+            responseSaleDTO.setUserId(saleDAO.getUserId());
+            responseSaleDTO.setImageUrl(saleDAO.getImageUrl());
+            responseSaleDTO.setIndividual(saleDAO.getIndividual());
+            responseSaleDTO.setFirstSpecies(saleDAO.getFirstSpecies());
+            responseSaleDTO.setSecondSpecies(saleDAO.getSecondSpecies());
+            responseSaleDTO.setHeartCount(saleDAO.getHeartCount());
+            responseSaleDTO.setBirthday(saleDAO.getBirthday());
+            responseSaleDTO.setWeight(saleDAO.getWeight());
+            responseSaleDTO.setSpeciesExplain(saleDAO.getSpeciesExplain());
+            responseSaleDTO.setPrice(saleDAO.getPrice());
+            responseSaleDTO.setHeartCount(saleDAO.getHeartCount());
+            responseSaleDTO.setSalePlatform(saleDAO.getSalePlatform().name());
+
+            responseSaleDTOs.add(responseSaleDTO);
+        }
+
+        // List 구조를 Page 구조로 변경 후 반환
+        return new PageImpl<>(responseSaleDTOs, pageable, saleDAOs.getTotalElements());
     }
 }
