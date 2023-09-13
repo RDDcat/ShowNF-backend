6224c98e85640d2fd9256cf6040d3eb74531e3b3
Lee <lsmin476@naver.com>
2023-09-13 15:58:23 +0900

    |SM UPDATE| DTO 분양글 조회를 위한 객체 생성

--- src/main/java/com/shownf/reptile/Model/DTO/ResponseSaleDTO.java
+++ src/main/java/com/shownf/reptile/Model/DTO/ResponseSaleDTO.java
@@ -1,2 +1,21 @@
-package com.shownf.reptile.Model.DTO;public class ResponseSaleDTO {
+package com.shownf.reptile.Model.DTO;
+
+import com.shownf.reptile.Model.Enum.SalePlatform;
+import lombok.Data;
+
+@Data
+public class ResponseSaleDTO {
+    Long saleId;
+    Long petId;
+    String userId;
+    String imageUrl;
+    String individual;
+    String firstSpecies;
+    String secondSpecies;
+    String birthday;
+    Double weight;
+    String speciesExplain;
+    Integer price;
+    Integer heartCount;
+    String salePlatform;
 }
