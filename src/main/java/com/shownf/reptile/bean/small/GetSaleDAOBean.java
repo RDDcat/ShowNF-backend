a6b9198ed2aa8c0f9c7430135401522e884b6733
Lee <lsmin476@naver.com>
2023-09-13 15:57:19 +0900

    |SM UPDATE| small Bean 분양글 DAO 가져오기

--- src/main/java/com/shownf/reptile/bean/small/GetSaleDAOBean.java
+++ src/main/java/com/shownf/reptile/bean/small/GetSaleDAOBean.java
@@ -1,2 +1,22 @@
-package com.shownf.reptile.bean.small;public class GetSaleDAOBean {
+package com.shownf.reptile.bean.small;
+
+import com.shownf.reptile.Model.entity.SaleDAO;
+import com.shownf.reptile.repository.SaleRepositoryJPA;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GetSaleDAOBean {
+
+    SaleRepositoryJPA saleRepositoryJPA;
+
+    @Autowired
+    public GetSaleDAOBean(SaleRepositoryJPA saleRepositoryJPA) {
+        this.saleRepositoryJPA = saleRepositoryJPA;
+    }
+
+    // 분양글 찾기
+    public SaleDAO exec(Long saleId){
+        return saleRepositoryJPA.findById(saleId).get();
+    }
 }
