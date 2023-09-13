d2f7f0284ba7759b5faca3fdae251f2f810ad2f9
Lee <lsmin476@naver.com>
2023-09-13 16:45:09 +0900

    |SM UPDATE| Repository 분양글 레포지토리 findAll 기능 추가

--- src/main/java/com/shownf/reptile/repository/SaleRepositoryJPA.java
+++ src/main/java/com/shownf/reptile/repository/SaleRepositoryJPA.java
@@ -1,7 +1,10 @@
 package com.shownf.reptile.repository;
 
 import com.shownf.reptile.Model.entity.SaleDAO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaleRepositoryJPA extends JpaRepository<SaleDAO, Long> {
+    Page<SaleDAO> findAll(Pageable pageable);
 }
